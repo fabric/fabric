f368d112b50d9e2deade61851b65ab91c12a879f
Christian Vest Hansen <karmazilla@gmail.com>
2008-01-06 22:37:29 +0100

    Added our own fabfile.py, plus a number of other small changes.

---
+++ fabfile.py
@@ -0,0 +1,26 @@
+
+# fabfile.py - A fabfile for Fabric itself.
+# Copyright (C) 2008  Christian Vest Hansen
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+def clean():
+    "Recurse the directory tree and remove all files matched by .gitignore."
+    local('cat .gitignore|xargs -I PATTERN '
+        + 'find . -name PATTERN -not -path "./.git/*" -delete')
+
+def release():
+    "Create a new release of Fabric, and upload it to our various services."
+    pass
