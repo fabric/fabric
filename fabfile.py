c53a0551c39db185d2f99220402484f2e7853fc6
Christian Vest Hansen <karmazilla@gmail.com>
2008-01-13 22:33:30 +0100

    Make ready_files use the MANIFEST to create a clean-room file set when doing a release.

--- fabfile.py
+++ fabfile.py
@@ -25,8 +25,20 @@ def clean():
 def ready_files():
     local('mkdir dist')
     set(prefix='fab-%(fab_version)s', filename='dist/%(prefix)s.tar.gz')
-    local('git archive --format=tar --prefix=%(prefix)s '
-        + '%(fab_version)s | gzip >%(filename)s')
+    # This next part needs explaining...
+    # 1. use Git to pack HEAD in TAR format to stdout, so we don't distribute
+    #    files with local changes. Plus, make sure that Git only packs those
+    #    files that are named in MANIFEST, so that we make sure that the
+    #    packages in the CheeseShop (PyPi) and on nongnu.org are similar.
+    # 2. Then we pipe the TAR data through gzip so we get it in tar.gz format.
+    # 3. Then, in the dist directory, we BOTH write data to a tar.gz file, AND
+    #    unpack it. This will create a clean-room file-set that we can use for
+    #    running distutils. This way, we also make sure that we don't get
+    #    uncommittet changes in our distutils package.
+    # Pretty clever, eh? Unix CLI-fu!
+    local('git archive --format=tar --prefix=%(prefix)s/ HEAD '
+            + '$(cat MANIFEST | perl -p -e "s/\\n/ /") | gzip | '
+            + '(cd dist && tee %(prefix)s.tar.gz | tar xzf -)')
     local('gpg -b --use-agent %(filename)s')
 
 def release(**kvargs):
@@ -35,12 +47,13 @@ def release(**kvargs):
     if not dry:
         local('git tag -s -m "Fabric v. %(fab_version)s" %(fab_version)s HEAD')
     ready_files()
-    scp_cmd = 'scp %(filename)s karmazilla@dl.sv.nongnu.org:/releases/fab/'
+    scp_cmd = 'scp $(filename) karmazilla@dl.sv.nongnu.org:/releases/fab/'
     if not dry:
         local(scp_cmd)
         set(filename='%(filename)s.sig')
         local(scp_cmd)
-    distutil_cmd = 'python setup.py sdist bdist upload --sign'
+    distutil_cmd = ('cd dist/%(prefix)s/ && '
+        + 'python setup.py sdist bdist upload --sign')
     if dry:
         distutil_cmd += ' --dry-run'
     local(distutil_cmd)
