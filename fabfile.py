14b44bd9b2567bca9f9a84e41a56beec9f061acb
Christian Vest Hansen <karmazilla@gmail.com>
2008-01-13 01:24:54 +0100

    Extended fafile.py with distutil calls and actual uploading to nongnu.org.

--- fabfile.py
+++ fabfile.py
@@ -22,10 +22,25 @@ def clean():
     local('cat .gitignore | xargs -I PATTERN '
         + 'find . -name PATTERN -not -path "./.git/*" | xargs rm -r')
 
-def release():
-    "Create a new release of Fabric, and upload it to our various services."
-    set(prefix='fab-%(fab_version)s')
-    local('git tag -s -m "Fabric v. %(fab_version)s" %(fab_version)s HEAD')
+def ready_files():
+    local('mkdir dist')
+    set(prefix='fab-%(fab_version)s', filename='dist/%(prefix)s.tar.gz')
     local('git archive --format=tar --prefix=%(prefix)s '
-        + '%(fab_version)s | gzip >%(prefix)s.tar.gz')
-    
+        + '%(fab_version)s | gzip >%(filename)s')
+    local('gpg -b --use-agent %(filename)s')
+
+def release(**kvargs):
+    "Create a new release of Fabric, and upload it to our various services."
+    dry = 'dry' in kvargs
+    if not dry:
+        local('git tag -s -m "Fabric v. %(fab_version)s" %(fab_version)s HEAD')
+    ready_files()
+    scp_cmd = 'scp %(filename)s karmazilla@dl.sv.nongnu.org:/releases/fab/'
+    if not dry:
+        local(scp_cmd)
+        set(filename='%(filename)s.sig')
+        local(scp_cmd)
+    distutil_cmd = 'python setup.py sdist bdist upload --sign'
+    if dry:
+        distutil_cmd += ' --dry-run'
+    local(distutil_cmd)
