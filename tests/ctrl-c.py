0fad7b35e523308408360b6f78f39680e2337446
Christian Vest Hansen <karmazilla@gmail.com>
2008-01-14 20:39:27 +0100

    Experiment with selector-api based console - inspired by a paramiko example.
    I'm not quite satisfied with it though. Needs more inteligent line handling, plus it
    currently screws the terminal and doesn't quite solve issue #1.

--- tests/ctrl-c.py
+++ tests/ctrl-c.py
@@ -17,26 +17,107 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 
-import sys
 import getpass
+import socket
+import sys
+
+# windows does not have termios...
+try:
+    import termios
+    import tty
+    has_termios = True
+except ImportError:
+    has_termios = False
 
 import paramiko
 
-client = paramiko.SSHClient()
+
+
+def posix_shell(prompt, *chans):
+    import select
+    
+    #oldtty = termios.tcgetattr(sys.stdin)
+    cmd_buf = ''
+    chan_bufs = {}
+    for chan in chans:
+        chan_bufs[chan.get_name()] = ''
+    try:
+        #tty.setraw(sys.stdin.fileno())
+        #tty.setcbreak(sys.stdin.fileno())
+        for chan in chans:
+            chan.settimeout(0.0)
+        selector_list = [sys.stdin]
+        for chan in chans:
+            selector_list.append(chan)
+        while True:
+            r, w, e = select.select(selector_list, [], [])
+            for chan in chans:
+                if chan in r:
+                    try:
+                        x = chan.recv(1024)
+                        name = chan.get_name()
+                        if len(x) == 0:
+                            print '\r\n*** EOF\r\n',
+                            return
+                        x = x.replace('\n', '\n[%s] ' % name)
+                        output = chan_bufs[name] + x
+                        lines = output.splitlines()
+                        if len(lines) > 1 and output.endswith('\n'):
+                            for line in lines:
+                                tpl = (name, line)
+                                sys.stdout.write('[%s] %s\n' % tpl)
+                            chan_bufs[name] = ''
+                        elif len(lines) > 1:
+                            for line in lines[:-1]:
+                                tpl = (name, line)
+                                sys.stdout.write('[%s] %s\n' % tpl)
+                            chan_bufs[name] = lines[-1]
+                        else:
+                            chan_bufs[name] = output
+                        sys.stdout.flush()
+                    except socket.timeout:
+                        pass
+            if sys.stdin in r:
+                x = sys.stdin.read(1)
+                if len(x) == 0:
+                    break
+                #sys.stdout.write(x)
+                cmd_buf += x
+                if x == '\n':
+                    sys.stdout.write(prompt)
+                    #selector_list = [sys.stdin]
+                    for chan in chans:
+                        stdin, stdout, stderr = chan.exec_command(cmd_buf)
+                        #stdout.get_name = (lambda: chan.get_name() + ' out')
+                        #stderr.get_name = (lambda: chan.get_name() + ' err')
+                        #selector_list.append(stdout)
+                        #selector_list.append(stderr)
+                    cmd_buf = ''
+                sys.stdout.flush()
+    finally:
+        pass
+        #termios.tcsetattr(sys.stdin, termios.TCSADRAIN, oldtty)
+
+
 try:
-    client.load_system_host_keys()
-    hostname = raw_input("Hostname: ")
     port = 22
-    username = raw_input("Username: ")
+    username = "UNWIRE\\cvh" #raw_input("Username: ")
     password = getpass.getpass()
-    
-    client.connect(hostname, port, username, password)
-    
-    cmd = raw_input("[%s@%s]$ " % (username, hostname))
-    stdin, stdout, stderr = client.exec_command(cmd)
-    for line in stdout:
-        print line,
+    ctor = paramiko.SSHClient
+    clients = [ctor(), ctor(), ctor()]
+    hosts = ['conan.unwire.dk', 'gekko.unwire.dk', 'atlas.unwire.dk']
+    channels = []
+    for num, client in enumerate(clients):
+        hostname = hosts[num]
+        client.load_system_host_keys()
+        client.connect(hostname, port, username, password)
+        channel = client.invoke_shell()
+        channel.set_name(hostname + ' out')
+        channel.setblocking(False)
+        channels.append(channel)
+    posix_shell("fab> ", *channels)
 except:
     sys.excepthook(*sys.exc_info())
 finally:
-    client.close()
+    for client in clients:
+        client.close()
