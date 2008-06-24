6c84d4c77f2e8555ca2fda67a2b3254eae45b588
Christian Vest Hansen <karmazilla@gmail.com>
2008-06-24 19:10:36 +0200

    Optimistically replace distutils.core with setuptools on Sergey Kirillovs suggestion.

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
 
 setup(
     name = 'Fabric',
