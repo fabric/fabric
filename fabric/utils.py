0b4fdcd7a4ebff6a585de2f4ca77391604d5cbe8
Jeff Forcier <jeff@bitprophet.org>
2009-04-17 17:38:20 -0400

    Ported invoke() but turned it into something that felt more elegant.

--- fabric/utils.py
+++ fabric/utils.py
@@ -79,3 +79,24 @@ def hosts(*host_list):
 
 def escape_quotes(string):
     return string.replace('"', r'\"')
+
+
+def runs_once(func):
+    """
+    Decorator preventing wrapped function from running more than once.
+
+    By keeping internal state, this decorator allows you to mark a function
+    such that it will only run once per Python interpreter session, which in
+    typical use means "once per invocation of the ``fab`` program".
+
+    Any function wrapped with this decorator will silently fail to execute the
+    2nd, 3rd, ..., Nth time it is called, and will return None in that instance.
+    """
+    @wraps(func)
+    def decorated(*args, **kwargs):
+        if hasattr(decorated, 'has_run'):
+            return
+        else:
+            decorated.has_run = True
+            return func(*args, **kwargs)
+    return decorated
