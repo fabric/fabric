63daa45fb9268e55fde3ca8f489f2066d27ba978
Jeff Forcier <jeff@bitprophet.org>
2009-05-02 19:45:04 -0400

    Cut 0.9 alpha 2

--- fabric/version.py
+++ fabric/version.py
@@ -6,7 +6,7 @@ problems with ``__init__.py`` (which is loaded by setup.py during installation,
 which in turn needs access to this version information.)
 """
 
-VERSION = (0, 9, 0, 'alpha', 1)
+VERSION = (0, 9, 0, 'alpha', 2)
 
 def get_version(verbose=False):
     """
