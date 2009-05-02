6177864c6f7093ff95ad711eff98a41bcf0ab02d
Jeff Forcier <jeff@bitprophet.org>
2009-05-02 19:22:37 -0400

    Update version machinery to allow for a 'verbose' version (for tagging)

--- fabric/version.py
+++ fabric/version.py
@@ -2,22 +2,41 @@
 Current Fabric version constant plus version pretty-print method.
 
 This functionality is contained in its own module to prevent circular import
-problems with ``__init__.py`` (which exhibits a public API, including `env`,
+problems with ``__init__.py`` (which is loaded by setup.py during installation,
 which in turn needs access to this version information.)
 """
 
 VERSION = (0, 9, 0, 'alpha', 1)
 
-# Version code inspired by Django (but modified to be setuptools/PyPI ready)
-def get_version():
+def get_version(verbose=False):
+    """
+    Return a version string for this package, based on `VERSION`.
+
+    When ``verbose`` is False (the default), `get_version` prints a
+    tag-friendly version of the string, e.g. '0.9a2'.
+
+    When ``verbose`` is True, a slightly more human-readable version is
+    produced, e.g. '0.9 alpha 2'.
+
+    This code is based off of Django's similar version output algorithm.
+    """
     # Major + minor only
     version = '%s.%s' % (VERSION[0], VERSION[1])
     # Append tertiary/patch if non-zero
     if VERSION[2]:
         version = '%s.%s' % (version, VERSION[2])
-    # Append alpha/beta modifier if not a final release (first char + number)
+    # Append alpha/beta modifier if not a final release
     if VERSION[3] != 'final':
-        version = '%s%s%s' % (version, VERSION[3][0], VERSION[4])
+        # If non-verbose, just the first letter of the modifier, and no spaces.
+        if not verbose:
+            version = '%s%s%s' % (version, VERSION[3][0], VERSION[4])
+        # Otherwise, be more generous.
+        else:
+            version = '%s %s %s' % (version, VERSION[3], VERSION[4])
+    # If it is final, and we're being verbose, also tack on the 'final'.
+    elif verbose:
+        version = '%s %s' % (version, VERSION[3])
+
     return version
 
 __version__ = get_version()
--- tests/test_version.py
+++ tests/test_version.py
@@ -8,11 +8,12 @@ import fabric.version
 
 
 def test_get_version():
-    for version, version_str in [
-        ((0, 2, 0, 'final'), '0.2'),
-        ((0, 2, 1, 'final'), '0.2.1'),
-        ((0, 2, 0, 'alpha', 1), '0.2a1'),
-        ((0, 2, 1, 'beta', 1), '0.2.1b1')
+    for version, version_str, verbose_version_str in [
+        ((0, 2, 0, 'final'), '0.2', '0.2 final'),
+        ((0, 2, 1, 'final'), '0.2.1', '0.2.1 final'),
+        ((0, 2, 0, 'alpha', 1), '0.2a1', '0.2 alpha 1'),
+        ((0, 2, 1, 'beta', 1), '0.2.1b1', '0.2.1 beta 1')
     ]:
         fabric.version.VERSION = version
         yield eq_, fabric.version.get_version(), version_str
+        yield eq_, fabric.version.get_version(verbose=True), verbose_version_str
