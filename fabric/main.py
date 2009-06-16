e17b88346be466845cf0967755cd5350e41122f3
Jeff Forcier <jeff@bitprophet.org>
2009-06-16 12:12:49 -0400

    Tried a fix to the "uh oh Fabric is earlier in sys.path than your fabfile!" bug

--- fabric/main.py
+++ fabric/main.py
@@ -98,13 +98,29 @@ def load_fabfile(path):
     directory, fabfile = os.path.split(path)
     # If the directory isn't in the PYTHONPATH, add it so our import will work
     added_to_path = False
+    index = None
     if directory not in sys.path:
         sys.path.insert(0, directory)
         added_to_path = True
+    # If the directory IS in the PYTHONPATH, move it to the front temporarily,
+    # otherwise other fabfiles -- like Fabric's own -- may scoop the intended
+    # one.
+    else:
+        i = sys.path.index(directory)
+        if i != 0:
+            # Store index for later restoration
+            index = i
+            # Add to front, then remove from original position
+            sys.path.insert(0, directory)
+            del sys.path[i + 1]
     # Perform the import (trimming off the .py)
     imported = __import__(os.path.splitext(fabfile)[0])
     # Remove directory from path if we added it ourselves (just to be neat)
     if added_to_path:
+        del sys.path[0]
+    # Put back in original index if we moved it
+    if index is not None:
+        sys.path.insert(index + 1, directory)
         del sys.path[0]
     # Return dictionary of callables only (and don't include Fab operations or
     # underscored callables)
