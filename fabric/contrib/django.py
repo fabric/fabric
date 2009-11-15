a5f54aedfc56a4ba4f4ecbe30239cbc12cdc7f72
Jeff Forcier <jeff@bitprophet.org>
2009-11-15 12:26:36 -0500

    Document contrib.django module more thoroughly.

--- fabric/contrib/django.py
+++ fabric/contrib/django.py
@@ -1,5 +1,58 @@
 """
-Functions providing integration with the Django web framework.
+.. versionadded:: 1.0
+
+These functions streamline the process of initializing Django's settings module
+environment variable. Once this is done, your fabfile may import from your
+Django project, or Django itself, without requiring the use of ``manage.py``
+plugins or having to set the environment variable yourself every time you use
+your fabfile.
+
+Currently, these functions only allow Fabric to interact with
+local-to-your-fabfile Django installations. This is not as limiting as it
+sounds; for example, you can use Fabric as a remote "build" tool as well as
+using it locally. Imagine the following fabfile::
+
+    from fabric.api import run, local, hosts, cd
+    from fabric.contrib import django
+
+    django.project('myproject')
+    from myproject.myapp.models import MyModel
+
+    def print_instances():
+        for instance in MyModel.objects.all():
+            print(instance)
+
+    @hosts('production-server')
+    def print_production_instances():
+        with cd('/path/to/myproject'):
+            run('fab print_instances')
+
+With Fabric installed on both ends, you could execute
+``print_production_instances`` locally, which would trigger ``print_instances``
+on the production server -- which would then be interacting with your
+production Django database.
+
+As another example, if your local and remote settings are similar, you can use
+it to obtain e.g. your database settings, and then use those when executing a
+remote (non-Fabric) command. This would allow you some degree of freedom even
+if Fabric is only installed locally::
+
+    from fabric.api import run
+    from fabric.contrib import django
+
+    django.settings_module('myproject.settings')
+    from django.conf import settings
+
+    def dump_production_database():
+        run('mysqldump -u %s -p=%s %s > /tmp/prod-db.sql' % (
+            settings.DATABASE_USER,
+            settings.DATABASE_PASSWORD,
+            settings.DATABASE_NAME
+        ))
+
+The above snippet will work if run from a local, development environment, again
+provided your local ``settings.py`` mirrors your remote one in terms of
+database connection info.
 """
 
 import os
@@ -17,17 +70,6 @@ def settings_module(module):
     This function provides a shortcut for doing so; call it near the top of
     your fabfile or Fabric-using code, after which point any Django imports
     should work correctly.
-
-    For example, to import some models from a Django project::
-
-        from fabric.contrib import django
-
-        django.settings_module('myproject.settings')
-        from myproject.myapp.models import MyModelClass
-
-        def print_models():
-            for instance in MyModelClass.objects.all():
-                print(instance.attribute)
 
     .. note::
 
@@ -47,16 +89,6 @@ def project(name):
     location.
 
     Uses `settings_module` -- see its documentation for details on why and how
-    to use this functionality. Another example (which would also work for
-    `settings_module`, as they are functionally equivalent)::
-
-        from fabric.contrib import django
-
-        django.project('myproject')
-        from django.conf import settings as django_settings
-
-        def print_database_info():
-            print("User: %s" % django_settings.DATABASE_USER)
-            print("Password: %s" % django_settings.DATABASE_PASSWORD)
+    to use this functionality.
     """
     settings_module('%s.settings' % name)
