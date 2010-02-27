3026f196f61ed6ac476243ac49e0920a02a4edab
Erich Heine <eheine@erich-heines-macbook-pro.local>
2010-02-26 21:04:47 -0600

    fabfile now contains way more tests, change to f1 to have contents
    the contents are needed to be able to distinquish when f1 has been
    copied to an existing (empty) file

--- test_scp/fabfile.py
+++ test_scp/fabfile.py
@@ -1,28 +1,70 @@
-from fabric.api import put, get
+from fabric.api import put, get, run, local, runs_once, env
+import os
 
+##################################################
+# admin stuff                                    #
+##################################################
+@runs_once
+def clean(dname='./*'):
+    env.warn_only = True
+    local('rm -r ' + dname)
+    env.warn_only = False
+
+##################################################
+# Tests of put()                                 #
+##################################################
 def cpr():
     'works fine'
-    put('./stuff/', '/tmp/stuff/', recursive=True)
+    put('./stuff/', '/tmp/foo/', recursive=True)
+
+def cpr2():
+    put('./*[!py]', "/tmp/foo", recursive=True)
+
+def cpr3():
+    put('.', '/tmp/foo', recursive=True)
+
+def cpr4():
+    d = os.getcwd()
+    put(d, '/tmp/foo', recursive=True)
+
+def cpr5():
+    os.chdir('../paramiko')
+    put('../test_scp/stuff/', '/tmp/foo', recursive=True)
+
+def cpg():
+    'works fine, but needs manual intervention for glob of *'
+    put('./stuff/*', '/tmp/foo', recursive=False)
+
 
 def cp():
-    'works fine, but needs manual intervention for glob of *'
-    put('./stuff/*', '/tmp/', recursive=False)
+    put('./stuff/f1','/tmp/foo/',recursive=False)
 
+def cp2():
+    run('touch /tmp/foo/file')
+    put('./stuff/f1','/tmp/foo/file',recursive=False)
+
+##################################################
+# Tests of get()                                 #
+##################################################
 def rcp():
-    '''
-    errors, to do this well we would probably need to muck with scp.py, not too
-    difficult actually....
-    '''
-    get('/tmp/stuff/*', './dl/', recursive=False)
+    get('/tmp/foo/stuff/f1', './f1')
 
 def rcp2():
-    '''
-    works just fine
-    '''
-    get('/tmp/stuff/d1/*', './dl/', recursive=False)
+    local('touch ./file')
+    get('/tmp/foo/stuff/f1', './file')
+
+def rcp3():
+    os.mkdir('testdir')
+    get('/tmp/foo/stuff/f1', 'testdir')
+
+
+def rcpg():
+    get('/tmp/foo/stuff/*', '.', recursive=False)
+
 
 def rcpr():
-    '''
-    works like a charm
-    '''
-    get('/tmp/stuff/', './dl/', recursive=True)
+    get('/tmp/foo/stuff/', '.', recursive=True)
+
+def rcpr2():
+    get('/tmp/foo/*[!py]', '.', recursive=True)
+
