93c3b1546cb3e13b3408846a714d1a1b98cf6a0d
Jeff Forcier <jeff@bitprophet.org>
2010-06-18 22:58:31 +0100

    Test tweaks for non-git environments.
    Why you'd be developing Fabric without Git installed, I have no idea. I
    guess for those users who like grabbing tarballs and running the test
    suite (valid, just unusual).

--- tests/test_version.py
+++ tests/test_version.py
@@ -9,7 +9,8 @@ import fabric.version
 
 def test_get_version():
     get_version = fabric.version.get_version
-    sha1 = "(%s)" % fabric.version.git_sha()
+    sha = fabric.version.git_sha()
+    sha1 = (" (%s)" % sha) if sha else ""
     for tup, short, normal, verbose in [
         ((0, 9, 0, 'final', 0), '0.9.0', '0.9', '0.9 final'),
         ((0, 9, 1, 'final', 0), '0.9.1', '0.9.1', '0.9.1 final'),
@@ -17,8 +18,8 @@ def test_get_version():
         ((0, 9, 1, 'beta', 1), '0.9.1b1', '0.9.1 beta 1', '0.9.1 beta 1'),
         ((0, 9, 0, 'release candidate', 1),
             '0.9rc1', '0.9 release candidate 1', '0.9 release candidate 1'),
-        ((1, 0, 0, 'alpha', 0), '1.0a %s' % sha1, '1.0 pre-alpha %s' % sha1,
-            '1.0 pre-alpha %s' % sha1),
+        ((1, 0, 0, 'alpha', 0), '1.0a%s' % sha1, '1.0 pre-alpha%s' % sha1,
+            '1.0 pre-alpha%s' % sha1),
     ]:
         fabric.version.VERSION = tup
         yield eq_, get_version('short'), short
