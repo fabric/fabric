dd7740a8dff97551a9994e1cbd232d18f8be3f03
Jeff Forcier <jeff@bitprophet.org>
2010-07-13 19:03:07 -0400

    Tweak 'fab test' to use rudolf

--- fabfile.py
+++ fabfile.py
@@ -21,10 +21,9 @@ def test(args=None):
 
     Specify string argument ``args`` for additional args to ``nosetests``.
     """
-    if args is None:
-        args = ""
-    local('nosetests -sv --with-doctest --nologcapture %s' % args,
-        capture=False)
+    default_args = "-sv --with-doctest --nologcapture --with-color"
+    default_args += (" " + args) if args else ""
+    local('nosetests %s' % default_args, capture=False)
 
 
 def build_docs(clean='no', browse='no'):
