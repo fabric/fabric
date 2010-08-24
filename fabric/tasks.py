7b89ce9b6675a66912d6a404f92f438e5e23f269
Erich Heine <eheine@mobile-107-87.near.uiuc.edu>
2010-08-24 17:26:38 -0500

    some crazy taskmodel decoration stuff

--- fabric/tasks.py
+++ fabric/tasks.py
@@ -1,3 +1,4 @@
+from functools import wraps
 
 class Task(object):
     """
@@ -26,4 +27,14 @@ class WrappedCallableTask(Task):
 
     def __call__(self, *args, **kwargs):
         return self.run(*args, **kwargs)
+
+def taskdecorator(taskmodifier):
+    @wraps(taskmodifier)
+    def taskfactory(target_task):
+        if not isinstance(target_task, Task):
+            target_task = WrappedCallableTask(target_task)
+        return taskmodifier(target_task)
+    return taskfactory
+
+
 
