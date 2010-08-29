b9554b6c3407be9e2885a351c010b31bfe885bac
Jeff Forcier <jeff@bitprophet.org>
2010-08-28 22:17:48 -0400

    Backport #151 from master to 0.9

--- fabric/utils.py
+++ fabric/utils.py
@@ -65,3 +65,60 @@ def indent(text, spaces=4, strip=False):
     # Reintroduce first indent (which just got stripped out)
     output = prefix + output
     return output
+
+
+def puts(text, show_prefix=True, end="\n", flush=False):
+    """
+    An alias for ``print`` whose output is managed by Fabric's output controls.
+
+    In other words, this function simply prints to ``sys.stdout``, but will
+    hide its output if the ``user`` :doc:`output level
+    </usage/output_controls>` is set to ``False``.
+
+    If ``show_prefix=False``, `puts` will omit the leading ``[hostname]``
+    which it tacks on by default. (It will also omit this prefix if
+    ``env.host_string`` is empty.)
+
+    Newlines may be disabled by setting ``end`` to the empty string (``''``).
+    (This intentionally mirrors Python 3's ``print`` syntax.)
+
+    You may force output flushing (e.g. to bypass output buffering) by setting
+    ``flush=True``.
+
+    .. versionadded:: 0.9.2
+    .. seealso:: `~fabric.utils.fastprint`
+    """
+    from fabric.state import output, env
+    if output.user:
+        prefix = ""
+        if env.host_string and show_prefix:
+            prefix = "[%s] " % env.host_string
+        sys.stdout.write(prefix + str(text) + end)
+        if flush:
+            sys.stdout.flush()
+
+
+def fastprint(text, show_prefix=False, end="", flush=True):
+    """
+    Print ``text`` immediately, without any prefix or line ending.
+
+    This function is simply an alias of `~fabric.utils.puts` with different
+    default argument values, such that the ``text`` is printed without any
+    embellishment and immediately flushed.
+
+    It is useful for any situation where you wish to print text which might
+    otherwise get buffered by Python's output buffering (such as within a
+    processor intensive ``for`` loop). Since such use cases typically also
+    require a lack of line endings (such as printing a series of dots to
+    signify progress) it also omits the traditional newline by default.
+
+    .. note::
+
+        Since `~fabric.utils.fastprint` calls `~fabric.utils.puts`, it is
+        likewise subject to the ``user`` :doc:`output level
+        </usage/output_controls>`.
+
+    .. versionadded:: 0.9.2
+    .. seealso:: `~fabric.utils.puts`
+    """
+    return puts(text=text, show_prefix=show_prefix, end=end, flush=flush)
