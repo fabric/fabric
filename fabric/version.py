a3ad869a20df3a1ec1dbc130e8d15dd7dc30c4a4
Jeff Forcier <jeff@bitprophet.org>
2011-02-18 16:12:28 -0800

    Cut 0.9.4 final

--- fabric/version.py
+++ fabric/version.py
@@ -6,7 +6,7 @@ problems with ``__init__.py`` (which is loaded by setup.py during installation,
 which in turn needs access to this version information.)
 """
 
-VERSION = (0, 9, 3, "final", 0)
+VERSION = (0, 9, 4, "final", 0)
 
 def get_version(form='short'):
     """
