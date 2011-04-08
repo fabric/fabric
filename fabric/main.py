598e0bac5fee54b7eab055a48604f6a3c51e7225
Morgan Goose <morgan.goose@gmail.com>
2011-04-08 13:46:28 -0400

    M the main env variable for ensure_order public

--- fabric/main.py
+++ fabric/main.py
@@ -365,7 +365,7 @@ def _merge(hosts, roles):
         role_hosts += value
 
     # Return deduped combo of hosts and role_hosts
-    if hasattr(state.env, '_ensure_order') and state.env._ensure_order:
+    if hasattr(state.env, 'ensure_order') and state.env.ensure_order:
         result_hosts = []
         for host in hosts + role_hosts:
             if host not in result_hosts:
@@ -397,7 +397,7 @@ def get_hosts(command, cli_hosts, cli_roles):
     if hasattr(command, '_ensure_order') and command._ensure_order:
         if hasattr(command, '_sorted') and command._sorted == True:
             state.env._sorted = command._sorted
-        state.env._ensure_order = command._ensure_order
+        state.env.ensure_order = command._ensure_order
 
     # Command line per-command takes precedence over anything else.
     if cli_hosts or cli_roles:
