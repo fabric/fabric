123311488bd1560801d1ea0cbc42f26c6de909a4
Travis Swicegood <development@domain51.com>
2011-06-25 13:57:53 -0500

    doing two separate logs instead of trying to handle them all in one automagically

--- fabric/logger.py
+++ fabric/logger.py
@@ -1,37 +1,28 @@
 import logging
+log = logging.getLogger("fabric")
+system_log = logging.getLogger("fabric-system")
 
 
-class Proxy(object):
-    def __init__(self, wraps):
-        self.wraps = wraps
-
-    def __call__(self, msg, host=None, *args, **kwargs):
-        if not "extra" in kwargs:
-            kwargs["extra"] = {}
-        if host:
-            msg = "[%s] %s" % (host, msg)
-            kwargs["extra"]["host"] = host
-        self.wraps(msg, *args, **kwargs)
-
-
-class Logger(object):
-    def __init__(self, *args, **kwargs):
-        self.log = logging.getLogger("fabric")
-        self.prefix = None
-
-    def __getattr__(self, name):
-        attr = getattr(self.log, name)
-        if name in ["debug", "info", "warn", "error"]:
-            return Proxy(attr)
-        return attr
-
-log = Logger()
-
-
-def activate_console_logging(level=logging.INFO):
+def configure_system_logging(level=logging.INFO):
     formatter = logging.Formatter("%(message)s")
+    console = logging.StreamHandler()
+    console.setLevel(level)
+    console.setFormatter(formatter)
+    system_log.addHandler(console)
+    system_log.setLevel(level)
+
+
+def configure_fabric_logging(level=logging.INFO):
+    formatter = logging.Formatter("[%(host)s] %(message)s")
     console = logging.StreamHandler()
     console.setLevel(level)
     console.setFormatter(formatter)
     log.addHandler(console)
     log.setLevel(level)
+
+
+# TODO: make sure regular handler doesn't dupe error handler
+# TODO: add a ColorFormatter
+def configure_logging(level=logging.INFO):
+    configure_system_logging(level)
+    configure_fabric_logging(level)
