d7de413334594d54d92e9148a214af7b66807d61
Travis Swicegood <development@domain51.com>
2011-07-01 13:19:03 -0500

    Handle case where `args[0]` is not callable.
    This code may go away.  Handling `args` is going to be error prone and
    could lead to very bad situations.  For example, consider this code:
    
        def helper():
            pass
    
        @task(helper)
        def real_task():
            pass
    
    In this case, `real_task` won't be wrapped, `helper` will.  I think this
    edge case is enough to remove the need to handle `args`, which will
    simplify the code for determining whether this was invoked.

--- fabric/decorators.py
+++ fabric/decorators.py
@@ -22,7 +22,7 @@ def task(*args, **kwargs):
 
     .. versionchanged:: 1.2.0.dev
     """
-    invoked = bool(not args or kwargs or len(args) > 1)
+    invoked = bool(not args or kwargs or len(args) > 1 or not callable(args[0]))
     task_class = kwargs.pop("task_class", tasks.WrappedCallableTask)
     if not invoked:
         func, args = args[0], ()
