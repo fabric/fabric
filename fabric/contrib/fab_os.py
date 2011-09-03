743cd276acd225b2a8f6607c521c770948b61806
barberj <barber.justin@gmail.com>
2011-09-02 22:18:30 -0400

    remove method working. added helpers isfile and isdir which would correlate to os.path.isfile and os.path.isdir.

--- fabric/contrib/fab_os.py
+++ fabric/contrib/fab_os.py
@@ -2,7 +2,8 @@
 module mimicking python os module. as pythonistas we should be comfortable
 interacting with the underlaying operating system using the os module.
 
-inspired by ali-akber saifee module rfsutil (https://github.com/alisaifee/fabric/blob/master/fabric/contrib/rfsutil.py). difference is this module doesn't open another channel to perform the os interactions. the underlying fabric channels are used 
+inspired by ali-akber saifee module rfsutil (https://github.com/alisaifee/fabric/blob/master/fabric/contrib/rfsutil.py). 
+difference is this module doesn't open another channel to perform the os interactions. the underlying fabric channels are used 
 """
 import datetime
 import time
@@ -17,6 +18,34 @@ class Cannot(Exception):
 
     def __str__(self):
         return 'Cannot %s `%s`: No such file or directory' % (self.change,self.to_change)
+
+def getFileType(path, use_sudo=False):
+    func = sudo if use_sudo else run
+    if exists(path):
+        with settings(hide('everything'), warn_only=True):
+            output = func("stat -Lc '%%F' '%s'" % path)
+            return output
+    raise Exception
+
+def isfile(path, use_sudo=False):
+    """
+    Return True if path is an existing regular file. This follows symbolic links, 
+    so both islink() and isfile() can be true for the same path.
+    """
+    try:
+        return 'file' in getFileType(path, use_sudo) 
+    except:
+        raise Cannot('isfile',path)
+
+def isdir(path, use_sudo=False):
+    """
+    Return True if path is an existing directory. This follows symbolic links, 
+    so both islink() and isdir() can be true for the same path.
+    """
+    try:
+        return 'directory' in getFileType(path, use_sudo) 
+    except:
+        raise Cannot('isdir',path)
 
 def stat(filename, use_sudo=False):
     """
@@ -56,3 +85,24 @@ def remove(path, use_sudo=False):
     """
 
     func = sudo if use_sudo else run
+    if exists(path):
+        #with settings(hide('everything'), warn_only=True):
+        with settings(warn_only=True):
+            if isfile(path):
+                print 'is file'
+                func('rm %s' % path) 
+            else:
+                print 'is dir'
+                raise OSError("[Errno 21] Is a directory: '%s'" % path)
+    else:
+        raise Cannot('remove',path)
+
+def removedirs(path, use_sudo=False):
+    """
+    Remove directories recursively. Works like rmdir() except that, if the leaf directory is successfully removed, 
+    removedirs() tries to successively remove every parent directory mentioned in path until an error is raised 
+    (which is ignored, because it generally means that a parent directory is not empty). 
+    For example, os.removedirs('foo/bar/baz') will first remove the directory 'foo/bar/baz', and then remove 'foo/bar' 
+    and 'foo' if they are empty. Raises OSError if the leaf directory could not be successfully removed.
+    """
+    pass
