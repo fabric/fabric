9e7832b066d381404f7d901e307a55b0694d5472
barberj <barber.justin@gmail.com>
2011-09-12 16:49:46 -0400

    flushed out more cases for isfile and created test isdir

--- tests/test_fabos.py
+++ tests/test_fabos.py
@@ -1,23 +1,46 @@
 from __future__ import with_statement
 
 from fabric.api import hide, get, show, run
-from fabric.contrib.fab_os import isfile 
+from fabric.contrib.fab_os import isfile, isdir
 
 from utils import FabricTest, eq_contents
 from fabric.state import env, output
 from server import server, FILES
 
-
-
 class TestFabOs(FabricTest):
-    # Make sure it knows / is a directory.
-    # This is in lieu of starting down the "actual honest to god fake operating
-    # system" road...:(
+
     @server(responses={"stat -Lc '%F' '/file.txt'":'regular file',
-            'test -e "/file.txt"':""
+            'test -e "/file.txt"':"",
+            "stat -Lc '%F' '/'":'directory',
+            'test -e "/"':"",
+            "stat -Lc '%F' 'junk'":"stat: cannot stat `junk'",
+            'test -e "junk"':""
     })
     def test_isfile(self):
         """
         isfile() returns string 'regular file'
         """
+        # Object is a file
         assert isfile('/file.txt') == True
+        # Object is a dir
+        assert isfile('/') == False
+        # Object doesn't exist 
+        assert isfile('junk') == False
+
+    @server(responses={"stat -Lc '%F' '/file.txt'":'regular file',
+            'test -e "/file.txt"':"",
+            "stat -Lc '%F' '/'":'directory',
+            'test -e "/"':"",
+            "stat -Lc '%F' 'junk'":"stat: cannot stat `junk'",
+            'test -e "junk"':""
+    })
+    def test_isdir(self):
+        """
+        isdir() returns string 'directory'
+        """
+        # Object is a file
+        assert isdir('/file.txt') == False
+        # Object is a dir 
+        assert isdir('/') == True
+        # Object doesn't exist 
+        assert isdir('junk') == False
