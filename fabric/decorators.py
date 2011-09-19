b9703f605dd13d175f95224a580ee05f57bd35ec
Jeff Forcier <jeff@bitprophet.org>
2011-09-19 12:44:29 -0700

    Remove unnecessary import

--- fabric/decorators.py
+++ fabric/decorators.py
@@ -4,7 +4,6 @@ Convenience decorators for use in fabfiles.
 from __future__ import with_statement
 
 from functools import wraps
-from types import StringTypes
 from Crypto import Random
 
 from fabric import tasks
