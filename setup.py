064c10e8d0fe729d0a6ec2571472e50470e47b7d
Jeff Forcier <jeff@bitprophet.org>
2011-09-21 16:51:43 -0700

    The multiprocessing fix is in Paramiko 1.7.7.1, don't need the fork
    Re #19

--- setup.py
+++ setup.py
@@ -38,10 +38,7 @@ setup(
     packages=find_packages(),
     test_suite='nose.collector',
     tests_require=['nose', 'fudge'],
-    install_requires=['pycrypto >= 1.9', 'paramiko==1.7.6.1'],
-    dependency_links = [
-        'https://github.com/goosemo/paramiko/tarball/master#egg=paramiko-1.7.6.1'
-    ],
+    install_requires=['paramiko>=1.7.7.1'],
     entry_points={
         'console_scripts': [
             'fab = fabric.main:main',
