a2a7528f631e465706126b246b255e12c49b0bb3
Jeff Forcier <jeff@bitprophet.org>
2011-11-23 14:12:47 -0800

    Update tagging mechanisms to have clean output

--- fabfile/tag.py
+++ fabfile/tag.py
@@ -1,5 +1,7 @@
 from __future__ import with_statement
 
+from contextlib import nested
+
 from fabric.api import *
 
 # Need to import this as fabric.version for reload() purposes
@@ -7,9 +9,13 @@ import fabric.version
 # But nothing is stopping us from making a convenient binding!
 _version = fabric.version.get_version
 
+from utils import msg
+
 
 def code_version_is_tagged():
-    return local('git tag | egrep "^%s$"' % _version('short'), capture=True)
+    with nested(hide('running'), msg("Searching for existing tag")):
+        cmd = 'git tag | egrep "^%s$"' % _version('short')
+        return local(cmd, capture=True)
 
 def update_code_version(force):
     """
@@ -19,22 +25,27 @@ def update_code_version(force):
     user quit without saving. Specify ``force=yes`` to override this.
     """
     version_file = "fabric/version.py"
-    raw_input("Work has been done since last tag, version update is needed. Hit Enter to load version info in your editor: ")
-    local("$EDITOR %s" % version_file)
+    raw_input("Version update required! Press Enter to load $EDITOR.")
+    with hide('running'):
+        local("$EDITOR %s" % version_file)
     # Try to detect whether user bailed out of the edit
-    if not local("git diff -- %s" % version_file, capture=True) and not force:
+    with hide('running'):
+        has_diff = local("git diff -- %s" % version_file, capture=True)
+    if not has_diff and not force:
         abort("You seem to have aborted the file edit, so I'm aborting too.")
     # Reload version module to get new version
     reload(fabric.version)
     # Commit the version update
-    local("git add %s" % version_file)
-    print("git commit -m \"Cut %s\"" % _version('normal'))
+    with msg("Committing updated version file to git"):
+        local("git add %s" % version_file)
+        local("git commit -m \"Cut %s\"" % _version('verbose'))
 
 def commits_since_tag():
     """
     Has any work been done since the last tag?
     """
-    return local("git log %s.." % _version('short'), capture=True)
+    with hide('running'):
+        return local("git log %s.." % _version('short'), capture=True)
 
 
 @task
@@ -70,11 +81,13 @@ def tag(force='no', push='no'):
         # At this point, we've incremented the in-code version and just need to
         # tag it in Git.
         f = 'f' if force else ''
-        local("git tag -%sam \"Fabric %s\" %s" % (
-            f,
-            _version('verbose'),
-            _version('short')
-        ))
+        with msg("Tagging"):
+            local("git tag -%sam \"Fabric %s\" %s" % (
+                f,
+                _version('normal'),
+                _version('short')
+            ))
         # And push to the central server, if we were told to
         if push.lower() in ['y', 'yes']:
-            local("git push origin %s" % _version('short'))
+            with msg("Pushing"):
+                local("git push origin %s" % _version('short'))
