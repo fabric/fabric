fdc27c889c1d95823d55b57d63d03b95dee4f423
Jeff Forcier <jeff@bitprophet.org>
2012-05-22 15:49:45 -0700

    Add failing test re #649

--- tests/test_parallel.py
+++ tests/test_parallel.py
@@ -1,13 +1,15 @@
 from __future__ import with_statement
 
-from fabric.api import run, parallel, env, hide
+from fabric.api import run, parallel, env, hide, execute
 
-from utils import FabricTest, eq_
+from utils import FabricTest, eq_, aborts
 from server import server, RESPONSES
 
 
-class TestParallel(FabricTest):
+class OhNoesException(Exception): pass
 
+
+class TestParallel(FabricTest):
     @server()
     @parallel
     def test_parallel(self):
@@ -18,3 +20,15 @@ class TestParallel(FabricTest):
         cmd = "ls /simple"
         with hide('everything'):
             eq_(run(cmd), RESPONSES[cmd])
+
+    @server(port=2200)
+    @server(port=2201)
+    @aborts
+    def test_parallel_failures(self):
+        with hide('everything'):
+            @parallel
+            def mytask():
+                run("ls /")
+                raise OhNoesException
+            
+            execute(mytask, hosts=['127.0.0.1:2200', '127.0.0.1:2201'])
