82d94d7dc307121046f54c7de323a6ddbd032cd3
Jeff Forcier <jeff@bitprophet.org>
2012-05-22 17:29:34 -0700

    Don't swallow exceptions in child processes.
    Also makes parallel execute() honor warn_only.
    
    Fixes #649

--- tests/test_parallel.py
+++ tests/test_parallel.py
@@ -1,10 +1,11 @@
 from __future__ import with_statement
 
-from fabric.api import run, parallel, env, hide, execute
+from fabric.api import run, parallel, env, hide, execute, settings
 
-from utils import FabricTest, eq_, aborts
+from utils import FabricTest, eq_, aborts, mock_streams
 from server import server, RESPONSES
 
+# TODO: move this into test_tasks? meh.
 
 class OhNoesException(Exception): pass
 
@@ -24,11 +25,34 @@ class TestParallel(FabricTest):
     @server(port=2200)
     @server(port=2201)
     @aborts
-    def test_parallel_failures(self):
+    def test_parallel_failures_abort(self):
         with hide('everything'):
+            host1 = '127.0.0.1:2200'
+            host2 = '127.0.0.1:2201'
+
             @parallel
             def mytask():
                 run("ls /")
-                raise OhNoesException
+                if env.host_string == host2:
+                    raise OhNoesException
             
-            execute(mytask, hosts=['127.0.0.1:2200', '127.0.0.1:2201'])
+            execute(mytask, hosts=[host1, host2])
+
+    @server(port=2200)
+    @server(port=2201)
+    @mock_streams('stderr') # To hide the traceback for now
+    def test_parallel_failures_honor_warn_only(self):
+        with hide('everything'):
+            host1 = '127.0.0.1:2200'
+            host2 = '127.0.0.1:2201'
+
+            @parallel
+            def mytask():
+                run("ls /")
+                if env.host_string == host2:
+                    raise OhNoesException
+
+            with settings(warn_only=True):
+                result = execute(mytask, hosts=[host1, host2])
+            eq_(result[host1], None)
+            assert isinstance(result[host2], OhNoesException)
