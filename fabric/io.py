c98695d68b23a8a6dbf350fc1850b392bd9c7909
Jeff Forcier <jeff@bitprophet.org>
2012-09-23 12:57:35 -0700

    Remove unneeded privatization of variables, re #733

--- fabric/io.py
+++ fabric/io.py
@@ -32,19 +32,18 @@ def output_loop(chan, attr, stream, capture):
 
 class OutputLooper(object):
     def __init__(self, chan, attr, stream, capture):
-        self._chan = chan
-        self._attr = attr
-        self._stream = stream
-        self._capture = capture
-        self._read_func = getattr(chan, attr)
-        self._prefix = "[%s] %s: " % (
+        self.chan = chan
+        self.stream = stream
+        self.capture = capture
+        self.read_func = getattr(chan, attr)
+        self.prefix = "[%s] %s: " % (
             env.host_string,
             "out" if attr == 'recv' else "err"
         )
-        self._printing = getattr(output, 'stdout' if (attr == 'recv') else 'stderr')
-        self._linewise = (env.linewise or env.parallel)
-        self._reprompt = False
-        self._read_size = 4096
+        self.printing = getattr(output, 'stdout' if (attr == 'recv') else 'stderr')
+        self.linewise = (env.linewise or env.parallel)
+        self.reprompt = False
+        self.read_size = 4096
 
     def loop(self):
         """
@@ -61,23 +60,23 @@ class OutputLooper(object):
 
         # Allow prefix to be turned off.
         if not env.output_prefix:
-            self._prefix = ""
+            self.prefix = ""
 
         while True:
             # Handle actual read
             try:
-                bytelist = self._read_func(self._read_size)
+                bytelist = self.read_func(self.read_size)
             except socket.timeout:
                 continue
             # Empty byte == EOS
             if bytelist == '':
                 # If linewise, ensure we flush any leftovers in the buffer.
-                if self._linewise and line:
-                    self._flush(self._prefix)
+                if self.linewise and line:
+                    self._flush(self.prefix)
                     self._flush("".join(line))
                 break
             # A None capture variable implies that we're in open_shell()
-            if self._capture is None:
+            if self.capture is None:
                 # Just print directly -- no prefixes, no capturing, nada
                 # And since we know we're using a pty in this mode, just go
                 # straight to stdout.
@@ -86,7 +85,7 @@ class OutputLooper(object):
             # prompts.
             else:
                 # Print to user
-                if self._printing:
+                if self.printing:
                     printable_bytes = bytelist
                     # Small state machine to eat \n after \r
                     if printable_bytes[-1] == "\r":
@@ -104,23 +103,23 @@ class OutputLooper(object):
                         printable_bytes = printable_bytes[cr.end(0):]
 
                         if not initial_prefix_printed:
-                            self._flush(self._prefix)
+                            self._flush(self.prefix)
 
                         if _has_newline(end_of_line):
                             end_of_line = ''
 
-                        if self._linewise:
+                        if self.linewise:
                             self._flush("".join(line)+end_of_line+"\n")
                             line = []
                         else:
                             self._flush(end_of_line+"\n")
                         initial_prefix_printed = False
 
-                    if self._linewise:
+                    if self.linewise:
                         line += [printable_bytes]
                     else:
                         if not initial_prefix_printed:
-                            self._flush(self._prefix)
+                            self._flush(self.prefix)
                             initial_prefix_printed = True
                         self._flush(printable_bytes)
 
@@ -128,13 +127,13 @@ class OutputLooper(object):
                 read_lines = re.split(r"(\r|\n|\r\n)", bytelist)
                 for fragment in read_lines:
                     # Store in capture buffer
-                    self._capture += fragment
+                    self.capture += fragment
                     # Store in internal buffer
                     _buffer += fragment
                     # Handle prompts
-                    prompt = _endswith(self._capture, env.sudo_prompt)
-                    try_again = (_endswith(self._capture, env.again_prompt + '\n')
-                        or _endswith(self._capture, env.again_prompt + '\r\n'))
+                    prompt = _endswith(self.capture, env.sudo_prompt)
+                    try_again = (_endswith(self.capture, env.again_prompt + '\n')
+                        or _endswith(self.capture, env.again_prompt + '\r\n'))
                     if prompt:
                         self.prompt()
                     elif try_again:
@@ -147,42 +146,42 @@ class OutputLooper(object):
         # backwards compatible with Fabric 0.9.x behavior; the user
         # will still see the prompt on their screen (no way to avoid
         # this) but at least it won't clutter up the captured text.
-        del self._capture[-1 * len(env.sudo_prompt):]
+        del self.capture[-1 * len(env.sudo_prompt):]
         # If the password we just tried was bad, prompt the user again.
-        if (not password) or self._reprompt:
+        if (not password) or self.reprompt:
             # Print the prompt and/or the "try again" notice if
             # output is being hidden. In other words, since we need
             # the user's input, they need to see why we're
             # prompting them.
-            if not self._printing:
-                self._flush(self._prefix)
-                if self._reprompt:
-                    self._flush(env.again_prompt + '\n' + self._prefix)
+            if not self.printing:
+                self._flush(self.prefix)
+                if self.reprompt:
+                    self._flush(env.again_prompt + '\n' + self.prefix)
                 self._flush(env.sudo_prompt)
             # Prompt for, and store, password. Give empty prompt so the
             # initial display "hides" just after the actually-displayed
             # prompt from the remote end.
-            self._chan.input_enabled = False
+            self.chan.input_enabled = False
             password = fabric.network.prompt_for_password(
-                prompt=" ", no_colon=True, stream=self._stream
+                prompt=" ", no_colon=True, stream=self.stream
             )
-            self._chan.input_enabled = True
+            self.chan.input_enabled = True
             # Update env.password, env.passwords if necessary
             set_password(password)
             # Reset reprompt flag
-            self._reprompt = False
+            self.reprompt = False
         # Send current password down the pipe
-        self._chan.sendall(password + '\n')
+        self.chan.sendall(password + '\n')
  
     def try_again(self):
         # Remove text from capture buffer
-        self._capture = self._capture[:len(env.again_prompt)]
+        self.capture = self.capture[:len(env.again_prompt)]
         # Set state so we re-prompt the user at the next prompt.
-        self._reprompt = True
+        self.reprompt = True
 
     def _flush(self, text):
-        self._stream.write(text)
-        self._stream.flush()
+        self.stream.write(text)
+        self.stream.flush()
 
 
 def input_loop(chan, using_pty):
