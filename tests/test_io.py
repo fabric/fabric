1683c2f825ae3d305dd8acd8fff46794a90fdc70
Nigel Owen <nigelowen2@gmail.com>
2012-10-05 23:33:21 +1300

    Applied some pep8 spaces which were lacking

--- tests/test_io.py
+++ tests/test_io.py
@@ -9,7 +9,7 @@ def test_request_prompts():
     ad = _AliasDict(
     {'request_prompts' : { "prompt2" : "response2","prompt1" : "response1" ,"prompt": "response" } }
         )
-    eq_(_get_prompt_response(list("this is a prompt for prompt1"),ad['request_prompts']),("prompt1","response1"))
-    eq_(_get_prompt_response(list("this is a prompt for prompt2"),ad['request_prompts']),("prompt2","response2"))
-    eq_(_get_prompt_response(list("this is a prompt for promptx:"),ad['request_prompts']),None)
-    eq_(_get_prompt_response(list("prompt for prom:"),ad['request_prompts']),None)
+    eq_(_get_prompt_response(list("this is a prompt for prompt1"), ad['request_prompts']), ("prompt1","response1"))
+    eq_(_get_prompt_response(list("this is a prompt for prompt2"), ad['request_prompts']), ("prompt2","response2"))
+    eq_(_get_prompt_response(list("this is a prompt for promptx:"), ad['request_prompts']), None)
+    eq_(_get_prompt_response(list("prompt for promp"), ad['request_prompts']), None)
