3f4314ea7e16339bf5c3f0fa9316fa581d818306
Jeff Forcier <jeff@bitprophet.org>
2012-10-19 15:29:08 -0700

    Clean up tasks tests to be less boilerplatey

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -8,7 +8,6 @@ import random
 import sys
 
 import fabric
-from fabric import tasks
 from fabric.tasks import WrappedCallableTask, execute, Task
 from fabric.api import run, env, settings, hosts, roles, hide, parallel
 from fabric.network import from_dict
@@ -19,12 +18,12 @@ from server import server
 
 
 def test_base_task_provides_undefined_name():
-    task = tasks.Task()
+    task = Task()
     eq_("undefined", task.name)
 
 @raises(NotImplementedError)
 def test_base_task_raises_exception_on_call_to_run():
-    task = tasks.Task()
+    task = Task()
     task.run()
 
 class TestWrappedCallableTask(unittest.TestCase):
@@ -52,48 +51,48 @@ class TestWrappedCallableTask(unittest.TestCase):
     def test_allows_any_number_of_args(self):
         args = [i for i in range(random.randint(0, 10))]
         def foo(): pass
-        task = tasks.WrappedCallableTask(foo, *args)
+        task = WrappedCallableTask(foo, *args)
 
     def test_allows_any_number_of_kwargs(self):
         kwargs = dict([("key%d" % i, i) for i in range(random.randint(0, 10))])
         def foo(): pass
-        task = tasks.WrappedCallableTask(foo, **kwargs)
+        task = WrappedCallableTask(foo, **kwargs)
 
     def test_run_is_wrapped_callable(self):
         def foo(): pass
-        task = tasks.WrappedCallableTask(foo)
-        self.assertEqual(task.wrapped, foo)
+        task = WrappedCallableTask(foo)
+        eq_(task.wrapped, foo)
 
     def test_name_is_the_name_of_the_wrapped_callable(self):
         def foo(): pass
         foo.__name__ = "random_name_%d" % random.randint(1000, 2000)
-        task = tasks.WrappedCallableTask(foo)
-        self.assertEqual(task.name, foo.__name__)
+        task = WrappedCallableTask(foo)
+        eq_(task.name, foo.__name__)
 
     def test_name_can_be_overridden(self):
         def foo(): pass
-        eq_(tasks.WrappedCallableTask(foo).name, 'foo')
-        eq_(tasks.WrappedCallableTask(foo, name='notfoo').name, 'notfoo')
+        eq_(WrappedCallableTask(foo).name, 'foo')
+        eq_(WrappedCallableTask(foo, name='notfoo').name, 'notfoo')
 
     def test_reads_double_under_doc_from_callable(self):
         def foo(): pass
         foo.__doc__ = "Some random __doc__: %d" % random.randint(1000, 2000)
-        task = tasks.WrappedCallableTask(foo)
-        self.assertEqual(task.__doc__, foo.__doc__)
+        task = WrappedCallableTask(foo)
+        eq_(task.__doc__, foo.__doc__)
 
     def test_dispatches_to_wrapped_callable_on_run(self):
         random_value = "some random value %d" % random.randint(1000, 2000)
         def foo(): return random_value
-        task = tasks.WrappedCallableTask(foo)
-        self.assertEqual(random_value, task())
+        task = WrappedCallableTask(foo)
+        eq_(random_value, task())
 
     def test_passes_all_regular_args_to_run(self):
         def foo(*args): return args
         random_args = tuple(
             [random.randint(1000, 2000) for i in range(random.randint(1, 5))]
         )
-        task = tasks.WrappedCallableTask(foo)
-        self.assertEqual(random_args, task(*random_args))
+        task = WrappedCallableTask(foo)
+        eq_(random_args, task(*random_args))
 
     def test_passes_all_keyword_args_to_run(self):
         def foo(**kwargs): return kwargs
@@ -101,30 +100,31 @@ class TestWrappedCallableTask(unittest.TestCase):
         for i in range(random.randint(1, 5)):
             random_key = ("foo", "bar", "baz", "foobar", "barfoo")[i]
             random_kwargs[random_key] = random.randint(1000, 2000)
-        task = tasks.WrappedCallableTask(foo)
-        self.assertEqual(random_kwargs, task(**random_kwargs))
+        task = WrappedCallableTask(foo)
+        eq_(random_kwargs, task(**random_kwargs))
 
     def test_calling_the_object_is_the_same_as_run(self):
         random_return = random.randint(1000, 2000)
         def foo(): return random_return
-        task = tasks.WrappedCallableTask(foo)
-        self.assertEqual(task(), task.run())
+        task = WrappedCallableTask(foo)
+        eq_(task(), task.run())
 
 
 class TestTask(unittest.TestCase):
     def test_takes_an_alias_kwarg_and_wraps_it_in_aliases_list(self):
         random_alias = "alias_%d" % random.randint(100, 200)
-        task = tasks.Task(alias=random_alias)
+        task = Task(alias=random_alias)
         self.assertTrue(random_alias in task.aliases)
 
     def test_aliases_are_set_based_on_provided_aliases(self):
         aliases = ["a_%d" % i for i in range(random.randint(1, 10))]
-        task = tasks.Task(aliases=aliases)
+        task = Task(aliases=aliases)
         self.assertTrue(all([a in task.aliases for a in aliases]))
 
     def test_aliases_are_None_by_default(self):
-        task = tasks.Task()
+        task = Task()
         self.assertTrue(task.aliases is None)
+
 
 # Reminder: decorator syntax, e.g.:
 #     @foo
