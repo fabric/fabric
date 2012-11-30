ee1efc7f88c4861b59837caea1e5450149ded6e7
Jeff Forcier <jeff@bitprophet.org>
2012-11-29 19:04:05 -0800

    Bump public Paramiko requirement for file-like object transfer feature

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ setup(
     packages=find_packages(),
     test_suite='nose.collector',
     tests_require=['nose', 'fudge<1.0'],
-    install_requires=['paramiko>=1.9.0'],
+    install_requires=['paramiko>=1.10.0'],
     entry_points={
         'console_scripts': [
             'fab = fabric.main:main',
