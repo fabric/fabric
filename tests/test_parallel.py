28e8246082f7584026475e89e01ee6238cdf2f11
Jeff Forcier <jeff@bitprophet.org>
2013-01-22 19:27:24 -0800

    Add regression test re #581

--- tests/test_parallel.py
+++ tests/test_parallel.py
@@ -3,7 +3,7 @@ from __future__ import with_statement
 from fabric.api import run, parallel, env, hide, execute, settings
 
 from utils import FabricTest, eq_, aborts, mock_streams
-from server import server, RESPONSES
+from server import server, RESPONSES, USER, HOST, PORT
 
 # TODO: move this into test_tasks? meh.
 
@@ -21,6 +21,21 @@ class TestParallel(FabricTest):
         cmd = "ls /simple"
         with hide('everything'):
             eq_(run(cmd), RESPONSES[cmd])
+
+    @server(port=2200)
+    @server(port=2201)
+    def test_env_host_no_user_or_port(self):
+        """
+        Ensure env.host doesn't get user/port parts when parallel
+        """
+        @parallel
+        def _task():
+            run("ls /simple")
+            assert USER not in env.host
+            assert str(PORT) not in env.host
+
+        host_string = '%s@%s:%%s' % (USER, HOST)
+        execute(_task, hosts=[host_string % 2200, host_string % 2201])
 
     @server(port=2200)
     @server(port=2201)
