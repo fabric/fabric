a3c34d82ff5fc46d98c4faa4bc9d2562c212a176
Jeff Forcier <jeff@bitprophet.org>
2013-03-21 17:36:22 -0700

    temporary troubleshooting

--- integration/test_operations.py
+++ integration/test_operations.py
@@ -9,10 +9,12 @@ from util import Integration
 
 class TestOperations(Integration):
     def test_no_trailing_space_in_shell_path_in_run(self):
-        run("mkdir -p /tmp/whatever/bin")
-        put(StringIO("#!/bin/bash\necho hi"), "/tmp/whatever/bin/myapp", mode="0755")
-        with path('/tmp/whatever/bin'):
-            assert run('myapp').stdout == 'hi'
+        from fabric.api import show
+        with show('debug'):
+            run("mkdir -p /tmp/whatever/bin")
+            put(StringIO("#!/bin/bash\necho hi"), "/tmp/whatever/bin/myapp", mode="0755")
+            with path('/tmp/whatever/bin'):
+                assert run('myapp').stdout == 'hi'
 
     def test_string_put_mode_arg_doesnt_error(self):
         put(StringIO("#!/bin/bash\necho hi"), "/tmp/whocares", mode="0755")
