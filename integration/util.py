fc6a0edca3ae42cb3570ddf62c841282bb0229aa
Jeff Forcier <jeff@bitprophet.org>
2013-05-26 15:53:26 -0700

    Allow easy local exec of integration suite via eg -H

--- integration/util.py
+++ integration/util.py
@@ -3,4 +3,5 @@ from fabric.api import env
 
 class Integration(object):
     def setup(self):
-        env.host_string = "127.0.0.1"
+        if not env.host_string: # Allow runtime selection
+            env.host_string = "127.0.0.1"
