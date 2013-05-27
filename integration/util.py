3eef3e39650436d631d16c3d3b19d4eab029cd29
Jeff Forcier <jeff@bitprophet.org>
2013-05-26 17:58:22 -0700

    Attempt to appease Travis environment

--- integration/util.py
+++ integration/util.py
@@ -3,8 +3,8 @@ from fabric.api import env
 
 class Integration(object):
     def setup(self):
-        if not env.host_string: # Allow runtime selection
-            env.host_string = "127.0.0.1"
+        # Just so subclasses can super() us w/o fear. Meh.
+        pass
 
     def teardown(self):
         # Just so subclasses can super() us w/o fear. Meh.
