f70823c9f6399e00abd74916f3e30418802ee616
Kevin Qiu <kevin@freshbooks.com>
2013-06-27 16:51:10 -0400

    Monkey-patch inspect.getargspec for Python 2.5
    because the return type changed from plain tuple to namedtuple since 2.6

--- fabric/tasks.py
+++ fabric/tasks.py
@@ -12,6 +12,26 @@ from fabric.job_queue import JobQueue
 from fabric.task_utils import crawl, merge, parse_kwargs
 from fabric.exceptions import NetworkError
 
+if sys.version_info[:2] == (2, 5):
+    # Python 2.5 inspect.getargspec returns a tuple
+    # instead of ArgSpec namedtuple.
+    class ArgSpec(object):
+        def __init__(self, args, varargs, keywords, defaults):
+            self.args = args
+            self.varargs = varargs
+            self.keywords = keywords
+            self.defaults = defaults
+            self._tuple = (args, varargs, keywords, defaults)
+
+        def __getitem__(self, idx):
+            return self._tuple[idx]
+
+    def patched_get_argspec(func):
+        return ArgSpec(*inspect._getargspec(func))
+
+    inspect._getargspec = inspect.getargspec
+    inspect.getargspec = patched_get_argspec
+
 
 def get_task_details(task):
     details = [task.__doc__ if task.__doc__ else 'No docstring provided']
