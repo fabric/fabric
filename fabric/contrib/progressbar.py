3221cdbd5279dfda9b5f570f809407edd19abd5b
Jakub Janczak <jakub.janczak@schibsted.pl>
2013-07-30 21:32:00 +0200

    The version of progressbar that shows the file name -much nicer when uploading/getting a directory

--- fabric/contrib/progressbar.py
+++ fabric/contrib/progressbar.py
@@ -1,31 +1,55 @@
 import sys
 from fabric.colors import *
 
-def show_progressbar(another_callback=None, progress_char='#', progress_color_function=green, show_percents=True, percents_color_function=yellow):
+def show_progressbar(another_callback=None, progress_char='#', progress_color_function=green, file_name_color_function=blue, show_percents=True, percents_color_function=yellow):
     number_of_columns = _get_terminal_size()[0] # for some reason the size of console is over by one
     is_already_finished = [False]
-    def show_progressbar_on_terminal(size_so_far, overall_size):
+    def show_progressbar_on_terminal(file_name, size_so_far, overall_size):
+        file_name_len = len(file_name)
+
         if not is_already_finished[0]:
             fraction_done = float(size_so_far)/overall_size
-            number_of_progress_chars_to_show = int(fraction_done*number_of_columns)
-            sys.stdout.write('\r' + progress_color_function(progress_char*number_of_progress_chars_to_show)),
+            progressbar_len = int(fraction_done*number_of_columns)
+
+            def progressbar_with_filename():
+                show_file_name_after_progressbar = file_name_len + 1 > progressbar_len
+                if show_file_name_after_progressbar:
+                    progress_to_show = progress_color_function(progress_char*progressbar_len) + ' ' + file_name_color_function(file_name)
+                    number_of_columns = progressbar_len + file_name_len + 1
+                else:
+                    progress_to_show = file_name_color_function(file_name) + ' ' + progress_color_function(progress_char*(progressbar_len - file_name_len - 1))
+                    number_of_columns = progressbar_len
+
+                return (progress_to_show, number_of_columns)
+
+            def progressbar_without_filename():
+                return (progress_color_function(progress_char*progressbar_len), progressbar_len)
+
+            can_show_file_name = file_name_len + progressbar_len + 1 < number_of_columns 
+            if can_show_file_name:
+                (progressbar, number_of_columns_taken) = progressbar_with_filename()
+            else:
+                (progressbar, number_of_columns_taken) = progressbar_without_filename()
 
             if show_percents:
                 percents_string = "%.2f%%" % (fraction_done*100)
                 percents_len = len(percents_string)
-                number_of_spaces_before_percents = number_of_columns - number_of_progress_chars_to_show - percents_len
+                number_of_spaces_before_percents = number_of_columns - number_of_columns_taken - percents_len
                 if (number_of_spaces_before_percents > 0):
-                    sys.stdout.write(' '*number_of_spaces_before_percents),
-                    sys.stdout.write(percents_color_function(percents_string)),
+                    progressbar += (' '*number_of_spaces_before_percents) + percents_color_function(percents_string)
 
             if size_so_far == overall_size:
-                sys.stdout.write("\n")
+                progressbar += '\n'
                 is_already_finished[0] = True
-                
+
+            sys.stdout.write('\r' + progressbar)
             sys.stdout.flush()
+        elif size_so_far != overall_size:
+            is_already_finished[0] = False
+            show_progressbar_on_terminal(file_name, size_so_far, overall_size)
 
     def progressbar_callback(file_name, size_so_far, overall_size):
-        show_progressbar_on_terminal(size_so_far, overall_size)
+        show_progressbar_on_terminal(file_name, size_so_far, overall_size)
         if (another_callback != None):
             another_callback(size_so_far, overall_size)
 
