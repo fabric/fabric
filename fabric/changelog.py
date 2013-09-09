cc91ca6f8dbcdddd3071a32b5a432068848a8466
Jeff Forcier <jeff@bitprophet.org>
2013-09-09 16:02:07 -0700

    Preserve description paragraphs

--- fabric/changelog.py
+++ fabric/changelog.py
@@ -37,9 +37,10 @@ def construct_releases(entries):
     for obj in reversed(entries):
         # The 'actual' intermediate object we want to focus on is wrapped first
         # in a LI, then a P.
-        focus = obj[0][0]
+        focus, rest = obj[0][0], obj[0][1:]
         # Releases 'eat' the entries in their line's list and get added to the
         # final data structure. They also inform new release-line 'buffers'.
+        # Release lines should have an empty 'rest' so it's ignored.
         if isinstance(focus, release):
             line = get_line(focus)
             # New release line/branch detected. Create it & dump unreleased into
@@ -58,7 +59,6 @@ def construct_releases(entries):
             # Existing line -> empty out its bucket into new release.
             # Skip 'major' bugs as those "belong" to the next release.
             else:
-                # FIXME: nuke this
                 releases.append({
                     'obj': focus,
                     'entries': [x for x in lines[line] if not x.major],
@@ -71,11 +71,16 @@ def construct_releases(entries):
         # * However, support/feature entries marked as 'backport' go into all
         # release lines as well, on the assumption that they were released to
         # all active branches.
+        # * The 'rest' variable (which here is the bug description, vitally
+        # important!) is preserved by stuffing it into the focus (issue)
+        # object.
         else:
             # Handle rare-but-valid non-issue-attached line items, which are
-            # always bugs.
+            # always bugs. (They are their own description.)
             if not isinstance(focus, issue):
-                focus = issue(type_='bug', nodelist=[focus], backported=False, major=False)
+                focus = issue(type_='bug', nodelist=[focus], backported=False, major=False, description=[focus])
+            else:
+                focus.attributes['description'] = rest
             # Bugs go errywhere
             if focus.type == 'bug' or focus.backported:
                 for line in lines:
@@ -113,9 +118,14 @@ def construct_nodes(releases):
         if not d['entries']:
             continue
         release = d['obj']
+        # Use docutils.nodes.Node.deepcopy to deepcopy the description nodes.
+        # If this is not done, multiple references to the same object (e.g. a
+        # reference object in the description of #649, which is then copied
+        # into 2 different release lists) will end up in the doctree, which
+        # makes subsequent parse steps very angry (index() errors).
         entries = [
             docutils.nodes.list_item('',
-                docutils.nodes.paragraph('', *x['nodelist'])
+                docutils.nodes.paragraph('', *(x['nodelist'] + map(lambda x: x.deepcopy(), x['description'])))
             )
             for x in d['entries']
         ]
