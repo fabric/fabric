b3352ec49ce3351a370b8c9b9831af456e38676b
David Halter <davidhalter88@gmail.com>
2013-11-12 15:25:40 +0100

    cleaned up tests, removed unnecessary _AliasDict, pep8

--- tests/test_io.py
+++ tests/test_io.py
@@ -1,31 +1,26 @@
 from nose.tools import eq_
-from fabric.state import _AliasDict
+
 from fabric.io import OutputLooper
-from fabric.state import env
+from fabric.context_managers import settings
 
 
 def test_request_prompts():
     """
     Test valid responses from prompts
     """
-    def run(txt, responses):
-        # try to fulfil the OutputLooper interface, only want to test
-        # _get_prompt_response.
-        old, env.prompts = env.prompts, responses
-        try:
+    def run(txt, prompts):
+        with settings(prompts=prompts):
+            # try to fulfil the OutputLooper interface, only want to test
+            # _get_prompt_response. (int has an attribute real)
             ol = OutputLooper(int, 'real', None, list(txt), None)
             return ol._get_prompt_response()
-        finally:
-            env.prompts = old
 
-    ad = _AliasDict({
-        'prompts': {
-            "prompt2": "response2",
-            "prompt1": "response1",
-            "prompt": "response"
-        }
-    })
-    eq_(run("this is a prompt for prompt1", ad['prompts']), ("prompt1", "response1"))
-    eq_(run("this is a prompt for prompt2", ad['prompts']), ("prompt2", "response2"))
-    eq_(run("this is a prompt for promptx:", ad['prompts']), None)
-    eq_(run("prompt for promp", ad['prompts']), None)
+    prompts = {"prompt2": "response2",
+               "prompt1": "response1",
+               "prompt": "response"
+               }
+
+    eq_(run("this is a prompt for prompt1", prompts), ("prompt1", "response1"))
+    eq_(run("this is a prompt for prompt2", prompts), ("prompt2", "response2"))
+    eq_(run("this is a prompt for promptx:", prompts), None)
+    eq_(run("prompt for promp", prompts), None)
