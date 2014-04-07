1b460d0d896645274c8d79f610bc28d04d8b329a
Jeff Forcier <jeff@bitprophet.org>
2014-04-06 17:22:50 -0700

    Add intersphinx back to www site.
    Also fix up sys.path munging to match www site's file

--- sites/docs/conf.py
+++ sites/docs/conf.py
@@ -1,7 +1,8 @@
 # Obtain shared config values
 import os, sys
-sys.path.append(os.path.abspath('..'))
-sys.path.append(os.path.abspath('../..'))
+from os.path import abspath, join, dirname
+sys.path.append(abspath(join(dirname(__file__), '..')))
+sys.path.append(abspath(join(dirname(__file__), '..', '..')))
 from shared_conf import *
 
 # Enable autodoc, intersphinx
@@ -10,9 +11,15 @@ extensions.extend(['sphinx.ext.autodoc', 'sphinx.ext.intersphinx'])
 # Autodoc settings
 autodoc_default_flags = ['members', 'special-members']
 
-# Intersphinx connection to stdlib
+# Default is 'local' building, but reference the public WWW site when building
+# under RTD.
+target = join(dirname(__file__), '..', 'www', '_build')
+if os.environ.get('READTHEDOCS') == 'True':
+    target = 'http://www.fabfile.org/'
+# Intersphinx connection to stdlib + www site
 intersphinx_mapping = {
     'python': ('http://docs.python.org/2.6', None),
+    'www': (target, None),
 }
 
 # Sister-site links to WWW
