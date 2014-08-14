bc0432b91e1dc53f1e896d5d755fcb15d75a37d6
Jeff Forcier <jeff@bitprophet.org>
2014-08-14 14:08:55 -0700

    Start fleshing out docstrings for Connection

--- fabric/connection.py
+++ fabric/connection.py
@@ -1,10 +1,42 @@
+# TODO: inherit from, or proxy to, invoke.context.Context
 class Connection(object):
+    """
+    A connection to an SSH daemon, with methods for commands and file transfer.
+    """
+
     def __init__(self, host, user=None, port=None):
         """
-        .. note::
-            If both shorthand and kwarg user/port are given, `.Connection`
-            `refuses the temptation to guess
-            <http://legacy.python.org/dev/peps/pep-0020/>`_ and instead raises
-            an exception.
+        Set up a new object representing a server connection.
+
+        :param str host:
+            the hostname (or IP address) of this connection. May include
+            shorthand for the ``user`` and/or ``port`` parameters, of the form
+            ``[user@]host[:port]``.
+
+        :param str user:
+            the login user for the remote connection. Defaults to your local
+            login username.
+
+        :param int port:
+            the remote port. Defaults to ``22``.
+
+        :raises ValueError:
+            if user or port values are given via both ``host`` shorthand *and*
+            their own arguments. (We `refuse the temptation to guess
+            <http://legacy.python.org/dev/peps/pep-0020/>`_.)
         """
         pass
+
+    def run(self, command):
+        """
+        Execute a shell command on the remote end of this connection.
+
+        :param str command:
+            The command to execute. Standard SSH daemons execute these
+            commands within the connecting user's login shell, so shell
+            semantics (pipes, redirects etc) may be used freely.
+
+        :returns:
+            A `~invoke.runner.Result` object containing the command's exit
+            status, stdout/err, etc.
+        """
