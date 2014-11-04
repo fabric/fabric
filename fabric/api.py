794f57b83449267229a369db52deceddb601ec79
Isaac Boehman <isaac@bitsighttech.com>
2014-11-04 12:30:11 -0500

    Adds local_tunnel context manager

--- fabric/api.py
+++ fabric/api.py
@@ -7,7 +7,7 @@ setup.py -- without triggering loads of most of the code. Which doesn't work so
 well when you're using setup.py to install e.g. ssh!
 """
 from fabric.context_managers import (cd, hide, settings, show, path, prefix,
-    lcd, quiet, warn_only, remote_tunnel, shell_env)
+    lcd, quiet, warn_only, local_tunnel, remote_tunnel, shell_env)
 from fabric.decorators import (hosts, roles, runs_once, with_settings, task,
         serial, parallel)
 from fabric.operations import (require, prompt, put, get, run, sudo, local,
