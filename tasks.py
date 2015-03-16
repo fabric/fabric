14b5658e1a26601c985dc95d04fb62c144574aeb
Jeff Forcier <jeff@bitprophet.org>
2015-03-16 11:17:46 -0700

    Align tasks.py docs stuff w/ invoke's

--- tasks.py
+++ tasks.py
@@ -6,23 +6,24 @@ from invocations.testing import test
 from invoke import Collection
 
 
-# TODO: move this & paramiko's copy of same into Alabaster?
+# TODO: move this & paramiko's copy of same into Alabaster? Invocations?
 
 
 d = 'sites'
 
-# Usage doc/API site (published as docs.paramiko.org)
+# Usage doc/API site (published as docs.fabfile.org)
 docs_path = join(d, 'docs')
-docs_build = join(docs_path, '_build')
 docs = Collection.from_module(_docs, name='docs', config={
-    'sphinx': {'source': docs_path, 'target': docs_build}
+    'sphinx': {'source': docs_path, 'target': join(docs_path, '_build')}
 })
+docs['build'].__doc__ = "Build the API docs subsite."
 
-# Main/about/changelog site ((www.)?paramiko.org)
+# Main/about/changelog site ((www.)?fabfile.org)
 www_path = join(d, 'www')
 www = Collection.from_module(_docs, name='www', config={
     'sphinx': {'source': www_path, 'target': join(www_path, '_build')}
 })
+www['build'].__doc__ = "Build the main project website."
 
 
 ns = Collection(docs=docs, www=www, test=test)
