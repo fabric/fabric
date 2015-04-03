0c9d6306a54355747399a398df3925aef6e362ff
Jeff Forcier <jeff@bitprophet.org>
2015-04-03 13:20:13 -0700

    Sudo thoughts

--- fabric/runners.py
+++ fabric/runners.py
@@ -18,4 +18,10 @@ class RemoteSudo(Remote):
     # * modify the command string (implies that's a subroutine or hooks based
     # thing)
     # * handle password prompting and playback (prob also a subroutine?)
+    # TODO: this may want to just become generic except-like handling (like fab
+    # 1 'prompts' stuff) in Remote, then this simply automates wrapping w/ sudo
+    # -c (i.e. one could get 100% same effect by manually doing run("sudo -c
+    # 'my command'")).
+    # TODO: that probably just means a method on Connection and no new class
+    # here.
     pass
