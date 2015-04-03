7ad073549439f26ddef702d233c536dd1e2903cf
Jeff Forcier <jeff@bitprophet.org>
2015-04-03 15:19:48 -0700

    Start skel tests for Connection open/close

--- tests/connection.py
+++ tests/connection.py
@@ -8,6 +8,10 @@ from fabric.utils import get_local_user
 
 
 class Connection_(Spec):
+    class basic_attributes:
+        def is_connected_defaults_to_False(self):
+            skip()
+
     class init:
         "__init__"
 
@@ -37,3 +41,44 @@ class Connection_(Spec):
 
             def may_be_given_as_kwarg(self):
                 eq_(Connection('host', port=2202).port, 2202)
+
+    class open:
+        def has_no_required_args_and_returns_None(self):
+            # Connection(host).open()
+            skip()
+
+        def calls_Client_connect(self):
+            "calls paramiko.Client.connect()"
+            # Mock Client somehow
+            skip()
+
+        def sets_is_connected_flag_when_successful(self):
+            # c = Connection(host)
+            # c.open()
+            # eq_(c.is_connected, True)
+            skip()
+
+        def is_connected_still_False_when_connect_fails(self):
+            skip()
+
+        def raises_some_sort_of_error_when_shit_explodes_idk(self):
+            # ???
+            skip()
+
+        # TODO: all the various connect-time options such as agent forwarding,
+        # host acceptance policies, how to auth, etc etc. These are all aspects
+        # of a given session and not necessarily the same for entire lifetime
+        # of a Connection object, should it ever disconnect/reconnect.
+        # TODO: though some/all of those things might want to be set to
+        # defaults at initialization time...
+
+    class close:
+        def calls_Client_close(self):
+            "calls paramiko.Client.close()"
+            skip()
+
+        def sets_is_connected_to_False(self):
+            skip()
+
+        def sets_is_connected_to_False_even_on_error(self):
+            skip()
