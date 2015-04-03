ccbc6db51b8ef5b9df2441636cbc8668bd0c0799
Jeff Forcier <jeff@bitprophet.org>
2015-04-03 13:17:27 -0700

    Skel bits for Runner tests

--- tests/runner.py
+++ tests/runner.py
@@ -4,14 +4,17 @@ from fabric.runner import Remote, RemoteSudo
 
 
 class Remote_(Spec):
-    # TODO: all other run() tests from fab1...
+    def needs_handle_on_a_Connection(self):
+        c = Connection('host')
+        ok_(Remote(context=c).context is c)
+
     def uses_paramiko_exec_command(self):
-        skip()
+        # * Patch Client.exec_command, right?
+        # * Run eg Remote(context=Connection('host')).run('command')
+        # * Assert exec_command called with 'command'
+        # * TODO: how does this truly tie in with run_direct/run_pty?
 
     def run_pty_uses_paramiko_get_pty(self):
-        skip()
-
-    def does_not_wrap_command_by_default(self):
         skip()
 
     def may_wrap_command_with_things_like_bash_dash_c(self):
@@ -21,6 +24,11 @@ class Remote_(Spec):
         # expose what it expects so run() can correctly determine things.
         # TODO: oughtn't this be part of invoke proper?
         skip()
+
+    def does_not_wrap_command_by_default(self):
+        skip()
+
+    # TODO: all other run() tests from fab1...
 
 
 class RemoteSudo_(Spec):
