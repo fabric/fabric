2e2a00a075c7f59375f90ee8b1416800dddd53d1
Jeff Forcier <jeff@bitprophet.org>
2015-04-05 21:42:14 -0700

    Move towards real spec usage

--- integration/main.py
+++ integration/main.py
@@ -1,48 +1,49 @@
-from spec import skip
+from spec import skip, Spec
 
 
-def simple_command_on_host():
-    """
-    Run command on host "localhost"
-    """
-    skip()
-    Connection('localhost').run('echo foo')
-    # => Result
+class Main(Spec):
+    def simple_command_on_host(self):
+        """
+        Run command on host "localhost"
+        """
+        skip()
+        Connection('localhost').run('echo foo')
+        # => Result
 
-def simple_command_on_multiple_hosts():
-    """
-    Run command on localhost...twice!
-    """
-    skip()
-    Batch(['localhost', 'localhost']).run('echo foo')
-    # => [Result, Result
+    def simple_command_on_multiple_hosts(self):
+        """
+        Run command on localhost...twice!
+        """
+        skip()
+        Batch(['localhost', 'localhost']).run('echo foo')
+        # => [Result, Result
 
-def sudo_command():
-    """
-    Run command via sudo on host "localhost"
-    """
-    skip()
-    Connection('localhost').sudo('echo foo')
+    def sudo_command(self):
+        """
+        Run command via sudo on host "localhost"
+        """
+        skip()
+        Connection('localhost').sudo('echo foo')
 
-def mixed_sudo_and_normal_commands():
-    """
-    Run command via sudo, and not via sudo, on "localhost"
-    """
-    skip()
-    cxn = Connection('localhost')
-    cxn.run('whoami')
-    cxn.sudo('whoami')
-    # Alternately...
-    cxn.run('whoami', runner=Basic)
-    cxn.run('whoami', runner=Sudo)
+    def mixed_sudo_and_normal_commands(self):
+        """
+        Run command via sudo, and not via sudo, on "localhost"
+        """
+        skip()
+        cxn = Connection('localhost')
+        cxn.run('whoami')
+        cxn.sudo('whoami')
+        # Alternately...
+        cxn.run('whoami', runner=Basic)
+        cxn.run('whoami', runner=Sudo)
 
-def switch_command_between_local_and_remote():
-    """
-    Run command truly locally, and over SSH via "localhost"
+    def switch_command_between_local_and_remote(self):
+        """
+        Run command truly locally, and over SSH via "localhost"
 
-    Only really makes sense at the task level though...
-    """
-    skip()
-    # Basic/raw
-    run('hostname') # Or Context().run('hostname')
-    Connection('localhost').run('hostname')
+        Only really makes sense at the task level though...
+        """
+        skip()
+        # Basic/raw
+        run('hostname') # Or Context().run('hostname')
+        Connection('localhost').run('hostname')
