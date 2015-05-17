85879faa6f6db5f98320c60a651c39cdfb89805b
Jeff Forcier <jeff@bitprophet.org>
2015-05-17 15:54:46 -0700

    Failing tests re: pty size

--- tests/utils.py
+++ tests/utils.py
@@ -3,9 +3,9 @@ Tests testing the fabric.utils module, not utils for the tests!
 """
 
 from mock import patch
-from spec import Spec, eq_
+from spec import Spec, eq_, skip
 
-from fabric.utils import get_local_user
+from fabric.utils import get_local_user, get_pty_size
 
 
 # Basically implementation tests, because it's not feasible to do a "real" test
@@ -22,3 +22,19 @@ class get_local_user_(Spec):
         eq_(get_local_user(), None)
 
     # TODO: test for ImportError+win32 once appveyor is set up as w/ invoke
+
+
+class pty_size(Spec):
+    # TODO: Windows tests under appveyor
+
+    def calls_fcntl_with_TIOCGWINSZ(self):
+        # Basic terrible impl test because yea
+        skip()
+
+    def defaults_to_80x24_when_stdout_lacks_fileno(self):
+        # i.e. when accessing it throws AttributeError
+        skip()
+
+    def defaults_to_80x24_when_stdout_not_a_tty(self):
+        # i.e. when os.isatty(sys.stdout) is False
+        skip()
