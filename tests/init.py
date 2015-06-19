88165084ff41827cf2ed3b2582c336670cf05a0a
Jeff Forcier <jeff@bitprophet.org>
2015-06-19 15:29:45 -0700

    Tests for top level api

---
+++ tests/init.py
@@ -0,0 +1,14 @@
+from spec import Spec, eq_, ok_
+
+import fabric
+from fabric import _version, connection
+
+
+class init(Spec):
+    "__init__"
+    def version_and_version_info(self):
+        for name in ('__version_info__', '__version__'):
+            eq_(getattr(_version, name), getattr(fabric, name))
+
+    def Connection(self):
+        ok_(fabric.Connection is connection.Connection)
