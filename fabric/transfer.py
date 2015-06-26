dfd329c2539fda8032b6fe05547f730c06088c58
Jeff Forcier <jeff@bitprophet.org>
2015-06-26 12:43:23 -0700

    Skeleton basic tests for get()

---
+++ fabric/transfer.py
@@ -0,0 +1,14 @@
+"""
+File transfer via SFTP and/or SCP.
+"""
+
+# TODO: figure out best way to direct folks seeking rsync, to patchwork's rsync
+# call (which needs updating to use invoke.run() & fab 2 connection methods,
+# but is otherwise suitable).
+# UNLESS we want to try and shoehorn it into this module after all? Delegate
+# any recursive get/put to it? Requires users to have rsync available of
+# course.
+
+
+class Transfer(object):
+    pass
---
+++ tests/transfer.py
@@ -0,0 +1,49 @@
+from spec import Spec, skip
+
+from fabric import transfer
+
+
+# TODO: pull in all edge/corner case tests from fabric v1
+
+class Transfer_(Spec):
+    # TODO: SFTP clear default, but how to do SCP? subclass? init kwarg?
+
+    class init:
+        "__init__"
+        def requires_connection(self):
+            # Transfer() -> explodes
+            # Transfer(Connection()) -> happy, exposes an attribute
+            skip()
+
+    class get:
+        def preserves_remote_mode_by_default(self):
+            # remote foo.txt is something unlikely to be default local
+            # umask (but still readable by ourselves) -> get() -> local
+            # file matches remote mode.
+            skip()
+
+        class no_local_path:
+            def remote_relative_path_to_local_cwd(self):
+                # cxn.get('foo.txt') -> ./foo.txt
+                skip()
+
+            def remote_absolute_path_to_local_cwd(self):
+                # cxn.get('/tmp/foo.txt') -> ./foo.txt
+                skip()
+
+        class has_local_path:
+            def remote_relative_path_to_local_relative_path(self):
+                # cxn.get('foo.txt', local='bar.txt') -> ./bar.txt
+                skip()
+
+            def remote_absolute_path_to_local_relative_path(self):
+                # cxn.get('/tmp/foo.txt', local='bar.txt') -> ./bar.txt
+                skip()
+
+            def remote_relative_path_to_local_absolute_path(self):
+                # cxn.get('foo.txt', local='/tmp/bar.txt') -> /tmp/bar.txt
+                skip()
+
+            def remote_absolute_path_to_local_absolute_path(self):
+                # cxn.get('/tmp/foo.txt', local='/tmp/bar.txt') -> /tmp/bar.txt
+                skip()
