6a4c1067fe80f65bede713e10eecdfcf415e35d4
Jeff Forcier <jeff@bitprophet.org>
2015-07-15 14:32:21 -0700

    First real stab at get() docstring

--- fabric/transfer.py
+++ fabric/transfer.py
@@ -20,7 +20,48 @@ class Transfer(object):
         self.connection = connection
 
     def get(self, remote, local=None):
+        """
+        Download a file from the current connection to the local filesystem.
+
+        :param str remote:
+            Remote file to download.
+
+            Must evaluate to a file (not a directory). May be relative (from
+            remote default CWD, typically connecting user's ``$HOME``) or
+            absolute.
+
+        :param str local:
+            Local path to store downloaded file in, or a file-like object.
+
+            **If a file-like object is given**, the contents of the remote file
+            are simply written into it.
+
+            .. note::
+                The file-like object is assumed to be in the caller's desired
+                state re: file position - no ``.seek(0)`` or similar is
+                performed.
+
+            **If a string is given**, it should be a path pointing to a local
+            directory or file and is subject to similar behavior as that seen
+            by common Unix utilities or OpenSSH's ``sftp`` or ``scp`` tools.
+            For example, if the local path is a directory, the remote path's
+            base filename will be added onto it (so ``get('foo/bar/file.txt',
+            '/tmp/')`` would result in creation or overwriting of
+            ``/tmp/file.txt``).
+
+            .. note::
+                When dealing with nonexistent file paths, normal Python file
+                handling concerns will be present - for example, giving a local
+                path containing non-leaf directories which do not exist, will
+                typically result in an `OSError`.
+        """
+        # TODO: how does this API change if we want to implement
+        # remote-to-remote file transfer?
+        # TODO: handle string interpolation bits, especially the default one,
+        # or at least think about how that would work re: split between single
+        # and multiple server targets.
+        # TODO: callback support
+        #
         # obtain connection.open_sftp()
         # call result .get(remote, local)
-        # TODO: callback support
         pass
