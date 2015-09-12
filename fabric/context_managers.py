23f3471ddf77db86565ba6d736c73d0f1e90e2bd
Mathias Ertl <mati@er.tl>
2015-09-12 18:31:12 +0200

    faster comparisson

--- fabric/context_managers.py
+++ fabric/context_managers.py
@@ -28,7 +28,7 @@ from fabric.state import output, win32, connections, env
 from fabric import state
 from fabric.utils import isatty
 
-if six.PY2:
+if six.PY2 is True:
     from contextlib import nested
 else:
     from contextlib import ExitStack
