aebd566cc4c20c9abc3af975bea5a243b2463773
Mathias Ertl <mati@er.tl>
2015-10-03 19:45:57 +0200

    prepare setup.py for fork

--- setup.py
+++ setup.py
@@ -13,12 +13,12 @@ with open('README.rst') as f:
     readme = f.read()
 
 long_description = """
+Fabric3 is a fork of `Fabric <http://fabfile.org>`_ to provide compatability
+with Python 3.4+. The port still works with Python 2.7. Any new releases of
+Fabric will also be released here.
+
 To find out what's new in this version of Fabric, please see `the changelog
 <http://fabfile.org/changelog.html>`_.
-
-You can also install the `in-development version
-<https://github.com/fabric/fabric/tarball/master#egg=fabric-dev>`_ using
-pip, with `pip install fabric==dev`.
 
 ----
 
@@ -29,17 +29,19 @@ pip, with `pip install fabric==dev`.
 For more information, please see the Fabric website or execute ``fab --help``.
 """ % (readme)
 
-install_requires=['paramiko>=1.10', 'six>=1.9.0']
+install_requires=['paramiko>=1.15.3', 'six>=1.9.0']
 
 
 setup(
-    name='Fabric',
+    name='Fabric3',
     version=get_version('short'),
-    description='Fabric is a simple, Pythonic tool for remote execution and deployment.',
+    description='Fabric is a simple, Pythonic tool for remote execution and deployment (py2.7/py3.4+ compatible fork).',
     long_description=long_description,
     author='Jeff Forcier',
     author_email='jeff@bitprophet.org',
-    url='http://fabfile.org',
+    maintainer='Mathias Ertl',
+    maintainer_email='mati@er.tl',
+    url='https://github.com/mathiasertl/fabric/',
     packages=find_packages(),
     test_suite='nose.collector',
     tests_require=['nose', 'fudge<1.0', 'jinja2'],
@@ -59,9 +61,8 @@ setup(
           'Operating System :: Unix',
           'Operating System :: POSIX',
           'Programming Language :: Python',
-          'Programming Language :: Python :: 2.5',
-          'Programming Language :: Python :: 2.6',
           'Programming Language :: Python :: 2.7',
+          'Programming Language :: Python :: 3.4',
           'Topic :: Software Development',
           'Topic :: Software Development :: Build Tools',
           'Topic :: Software Development :: Libraries',
