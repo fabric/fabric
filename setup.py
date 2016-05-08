4dcf5933fa8dca8dd00f483c012b32e53f58a1ed
Jeff Forcier <jeff@bitprophet.org>
2016-05-08 14:11:48 -0700

    Pin test requirements

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
     url='http://fabfile.org',
     packages=find_packages(),
     test_suite='nose.collector',
-    tests_require=['nose', 'fudge<1.0', 'jinja2'],
+    tests_require=['nose<2.0', 'fudge<1.0', 'jinja2<3.0'],
     install_requires=install_requires,
     entry_points={
         'console_scripts': [
