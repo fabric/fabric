6695422fd2885a4ac78ce23c51d2a6fc26c6c0d1
Hamdi Sahloul <sahloul@race.u-tokyo.ac.jp>
2016-07-31 23:52:47 +0900

    Update files.py

--- fabric/contrib/files.py
+++ fabric/contrib/files.py
@@ -28,11 +28,7 @@ def exists(path, use_sudo=False, verbose=False):
     """
     func = use_sudo and sudo or run
     with settings(hide('everything'), warn_only=True):
-        try:
-            result = func("ver") #is there better way to detect remote windows?
-            cmd = 'stat %s' % (_expand_path(path) if result.failed else path)
-        except:
-            cmd = 'test -e %s' % _expand_path(path)
+        cmd = 'stat %s' % (_expand_path(path) if func("ver").failed else path)
     # If verbose, run normally
     if verbose:
         with settings(warn_only=True):
