85c1184f7665bebce190e288afb16d7d49004241
Jeff Forcier <jeff@bitprophet.org>
2016-08-01 17:14:35 -0700

    Allow using Paramiko<2 for the time being.
    Makes it easier to e.g. test Paramiko-on-Py3 things

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ setup(
     install_requires=[
         # TODO: pinning
         'invoke>=0.13,<2.0',
-        'paramiko>=2.0,<3.0',
+        'paramiko>=1.16,<3.0',
         'cryptography>=1.1',
     ],
     packages=['fabric'],
