8920abdf4b9d41a9ed85d32ac5aaf62a2bedea85
Hamdi Sahloul <sahloul@race.u-tokyo.ac.jp>
2016-08-09 03:29:58 +0900

    Update files.py

--- fabric/contrib/files.py
+++ fabric/contrib/files.py
@@ -449,7 +449,7 @@ def is_win():
     Return True if remote SSH server is running Windows OS, False otherwise
 
     The idea is based on echoing quoted text!
-    *NIX systems will echo quote text only,
+    \*NIX systems will echo quote text only,
     while windows echoes quotation marks as well.
     """
     with settings(hide('everything'), warn_only=True):
