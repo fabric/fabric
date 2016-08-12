a87178e8942f69003d72d7779a5e096a77d5c0e1
Jeff Forcier <jeff@bitprophet.org>
2016-08-11 17:02:01 -0700

    MVP local port forwarding.
    Based on but heavily reorganizing work from v1,
    including fabric#1218

--- fabric/connection.py
+++ fabric/connection.py
@@ -1,3 +1,9 @@
+from contextlib import contextmanager
+from threading import Thread, Event
+import errno
+import select
+import socket
+
 from invoke import Context
 from invoke.config import Config as InvokeConfig, merge_dicts
 from invoke.vendor import six
@@ -374,6 +380,212 @@ class Connection(Context):
         all details.
         """
         return Transfer(self).put(*args, **kwargs)
+
+    # TODO: finalize API names/nomenclature, "forwarding" is really confusing
+    # always, how best to mitigate that exactly?
+    # TODO: clean up docstrings
+    # TODO: yield the socket for advanced users? Other advanced use cases
+    # (perhaps factor out socket creation itself)?
+    # TODO: probably push some of this down into Paramiko
+    @contextmanager
+    def forward_local(self, local_port, remote_port=None,
+        remote_host='localhost', local_host='localhost'):
+        """
+        Open a tunnel connecting ``local_port`` to the server's environment.
+
+        For example, say you want to connect to a remote PostgreSQL database
+        which is locked down and only accessible via the system it's running
+        on. You have SSH access to this server, so you can temporarily make
+        port 5432 on your local system act like port 5432 on the server::
+
+            import psycopg2
+            from fabric import Connection
+
+            with Connection('my-db-server').forward_local(5432):
+                db = psycopg2.connect(
+                    host='localhost', port=5432, database='mydb'
+                )
+                # Do things with 'db' here
+
+        This method is analogous to using the ``-L`` option of OpenSSH's
+        ``ssh`` program.
+
+        :param int local_port: The local port number on which to listen.
+
+        :param str local_host:
+            The local hostname/interface on which to listen. Default:
+            ``localhost``.
+
+        :param int remote_port:
+            The remote port number. Defaults to the same value as
+            ``local_port``.
+
+        :param str remote_host:
+            The remote hostname serving the forwarded remote port. Default:
+            ``localhost`` (i.e., the host this `.Connection` is connected to.)
+
+        :returns:
+            Nothing; this method is only useful as a context manager affecting
+            local operating system state.
+        """
+        self.open()
+        if not remote_port:
+            remote_port = local_port
+
+        # Listener does all of the work, sitting in the background (so we can
+        # yield) and spawning threads every time somebody connects to our local
+        # port.
+        # TODO: rename to something like TunnelManager?
+        finished = Event()
+        listener = Listener(
+            local_port=local_port, local_host=local_host,
+            remote_port=remote_port, remote_host=remote_host,
+            # TODO: not a huge fan of handing in our transport, but...?
+            transport=self.transport, finished=finished,
+        )
+        listener.start()
+
+        # Return control to caller now that things ought to be operational
+        try:
+            yield
+        # Teardown once user exits block
+        finally:
+            # Signal to listener that it should close all open tunnels
+            finished.set()
+            # Then wait for it to do so
+            listener.join()
+            # TODO: raise any errors encountered inside thread
+            # TODO: cancel port forward on transport? Does that even make sense
+            # here (where we used direct-tcpip) vs the opposite method (which
+            # is what uses forward-tcpip)?
+
+
+    def forward_remote(self, xxx):
+        pass
+
+
+# TODO: inherit from error-handling thread
+class Listener(Thread):
+    def __init__(self,
+        local_host, local_port,
+        remote_host, remote_port,
+        transport, finished
+    ):
+        super(Listener, self).__init__()
+        self.local_address = (local_host, local_port)
+        self.remote_address = (remote_host, remote_port)
+        self.transport = transport
+        self.finished = finished
+
+    def run(self):
+        # Track each tunnel that gets opened during our lifetime
+        tunnels = []
+
+        # Set up OS-level listener socket on forwarded port
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # TODO: why do we want REUSEADDR exactly? and is it portable?
+        sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        # NOTE: choosing to deal with nonblocking semantics and a fast loop,
+        # versus an older approach which blocks & expects outer scope to cause
+        # a socket exception by close()ing the socket.
+        sock.setblocking(0)
+        sock.bind(self.local_address)
+        sock.listen(1)
+
+        while not self.finished.is_set():
+            # Main loop-wait: accept connections on the local listener
+            # NOTE: EAGAIN means "you're nonblocking and nobody happened to
+            # connect at this point in time"
+            try:
+                tun_sock, local_addr = sock.accept()
+            except socket.error as e:
+                if e.errno is errno.EAGAIN:
+                    import time; time.sleep(0.01)
+                    continue
+                raise
+
+            # Set up direct-tcpip channel on server end
+            # TODO: refactor w/ what's used for gateways
+            channel = self.transport.open_channel(
+                'direct-tcpip',
+                self.remote_address,
+                # TODO: realtalk, why isn't THIS just (local_host, local_port)?!
+                # Can't Paramiko's own threading do all this shit for us?
+                local_addr,
+            )
+
+            # Set up 'worker' thread for this specific connection to our
+            # tunnel, plus its dedicated signal event (which will appear as a
+            # public attr, no need to track both independently).
+            finished = Event()
+            tunnel = Tunnel(channel=channel, sock=tun_sock, finished=finished)
+            tunnel.start()
+            tunnels.append(tunnel)
+
+        # Propogate shutdown signal to all tunnels & wait for closure
+        # TODO: would be nice to have some output or at least logging here,
+        # especially for "sets up a handful of tunnels" use cases like
+        # forwarding nontrivial HTTP traffic.
+        for tunnel in tunnels:
+            tunnel.finished.set()
+            # TODO: handle in-thread errors
+            tunnel.join()
+
+        # All we have left to close is our own sock.
+        # TODO: handle errors?
+        sock.close()
+
+
+# TODO: inherit from the 'safe' error-handling thread class in Invoke?
+class Tunnel(Thread):
+    """
+    Thread that forwards data between an SSH channel and a local socket.
+    """
+    def __init__(self, channel, sock, finished):
+        self.channel = channel
+        self.sock = sock
+        self.finished = finished
+        super(Tunnel, self).__init__()
+
+    def run(self):
+        try:
+            while not self.finished.is_set():
+                # TODO: not x-platform...or is it??
+                r, w, x = select.select([self.sock, self.channel], [], [], 1)
+                # TODO: This can be generalized more, I think
+                if self.sock in r:
+                    # TODO: make configurable, possibly reusing other
+                    # parameterizations for similar things
+                    data = self.sock.recv(1024)
+                    if len(data) == 0:
+                        break
+                    self.channel.sendall(data)
+                if self.channel in r:
+                    # TODO: ditto
+                    data = self.channel.recv(1024)
+                    if len(data) == 0:
+                        break
+                    self.sock.sendall(data)
+        except socket.error, e:
+            # Sockets return bad file descriptor if closed.
+            # Maybe there is a cleaner way of doing this?
+            # TODO: do we really want these now that we're using a threading
+            # Event to signal shutdown? Feels like they might be due to the
+            # "yank plug on socket/channel" style of shutdown from v1 impl
+            if e.errno not in (socket.EBADF, errno.ECONNRESET):
+                raise
+
+        # TODO: turn both of these into one loop
+        # TODO: and probably don't try/except socket.errors...ugh
+        try:
+            self.channel.close()
+        except socket.error:
+            pass
+
+        try:
+            self.sock.close()
+        except socket.error:
+            pass
 
 
 class Group(list):
