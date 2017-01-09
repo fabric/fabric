83614c00677fd00040cb462f62ddeeaf80d694de
Jeff Forcier <jeff@bitprophet.org>
2017-01-09 14:35:44 -0800

    Skel tests for more corner cases re: exception handling

--- tests/connection.py
+++ tests/connection.py
@@ -715,10 +715,23 @@ class Connection_(Spec):
         def tunnel_manager_errors_bubble_up(self):
             self._thread_error('listener')
 
-
-        # TODO: this requires additional refactoring of _forward_local to be
+        # TODO: these require additional refactoring of _forward_local to be
         # more like the decorators in _util
         def multiple_tunnels_can_be_open_at_once(self):
+            skip()
+
+        def multiple_tunnel_errors_appear_in_thread_exception(self):
+            # E.g. two tunnels opened via nested contextmanagers (or modern
+            # style multi context managers, or contextlib.nested(), etc etc)
+            # both have tunnel-level errors; a ThreadingException should appear
+            # whose .exceptions contains both.
+            skip()
+
+        def mixed_tunnel_and_listener_errors_appear_in_thread_exception(self):
+            # E.g. with a single tunnel having both a tunnel error and a (at
+            # close time, since an exception at open time would mean the tunnel
+            # never runs) listener/manager error, the resulting ThreadException
+            # contains two exceptions.
             skip()
 
     class forward_remote:
