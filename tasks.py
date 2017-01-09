f6f6ae78a865a3c8bcb28c16157168470264e59b
Jeff Forcier <jeff@bitprophet.org>
2017-01-09 12:14:18 -0800

    Use new invocations error counter for testing thread sleeps etc

--- tasks.py
+++ tasks.py
@@ -1,5 +1,5 @@
 from invocations.docs import docs, www, sites, watch_docs
-from invocations.testing import test, integration, coverage, watch_tests
+from invocations.testing import test, integration, coverage, watch_tests, count_errors
 from invocations import packaging
 
 from invoke import Collection
@@ -8,7 +8,7 @@ from invoke.util import LOG_FORMAT
 
 ns = Collection(
     docs, www, test, coverage, integration, sites, watch_docs,
-    watch_tests, release=packaging,
+    watch_tests, count_errors, release=packaging,
 )
 ns.configure({
     'tests': {
