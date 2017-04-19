7237985c0a5b79f8307fc1f3d2cc0b2202b631db
Jeff Forcier <jeff@bitprophet.org>
2017-04-18 19:29:22 -0700

    Bunch of packaging bullshit

--- tasks.py
+++ tasks.py
@@ -2,7 +2,7 @@ from invocations.docs import docs, www, sites, watch_docs
 from invocations.testing import (
     test, integration, coverage, watch_tests, count_errors,
 )
-from invocations import packaging
+from invocations.packaging import release
 
 from invoke import Collection
 from invoke.util import LOG_FORMAT
@@ -10,11 +10,16 @@ from invoke.util import LOG_FORMAT
 
 ns = Collection(
     docs, www, test, coverage, integration, sites, watch_docs,
-    watch_tests, count_errors, release=packaging,
+    watch_tests, count_errors, release,
 )
 ns.configure({
     'tests': {
         'package': 'fabric',
         'logformat': LOG_FORMAT,
-    }
+    },
+    'packaging': {
+        'sign': True,
+        'wheel': True,
+        'check_desc': True,
+    },
 })
