e705e4c0b20cc29fc0bd7bd225b3727fa395aab4
Ryan P Kilby <rpkilby@ncsu.edu>
2018-02-10 16:50:56 -0500

    Add connection-based fabric Task

--- fabric/__init__.py
+++ fabric/__init__.py
@@ -2,4 +2,5 @@
 from ._version import __version_info__, __version__
 from .connection import Config, Connection
 from .runners import Remote, Result
+from .tasks import local_task, task, LocalTask, Task
 from .group import Group, SerialGroup, ThreadingGroup, GroupResult
