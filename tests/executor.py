4b8fa0f90fe3fb937f1f1dc75a6ea257acfe309d
Jeff Forcier <jeff@bitprophet.org>
2018-07-09 12:15:04 -0700

    Refactor + next test

--- tests/executor.py
+++ tests/executor.py
@@ -1,18 +1,20 @@
 from invoke import Collection, Context
 from invoke.parser import ParseResult, ParserContext, Argument
 from fabric import Executor, Task, Connection
+from fabric.exceptions import NothingToDo
 
 from mock import Mock
-from pytest import skip  # noqa
+from pytest import skip, raises  # noqa
 
 
-def _execute_task(val=None, post=None):
+def _execute(val=None, post=None, remainder=""):
     post_tasks = []
     if post is not None:
         post_tasks.append(post)
     hosts = Argument(name="hosts")
     hosts.value = val
     core_args = ParseResult([ParserContext(args=[hosts])])
+    core_args.remainder = remainder
     task = Mock()
     coll = Collection(mytask=Task(task, post=post_tasks))
     executor = Executor(coll, core=core_args)
@@ -24,25 +26,26 @@ class Executor_:
     class expand_calls:
         class hosts_flag_empty:
             def no_parameterization_is_done(self):
-                task = _execute_task()
+                task = _execute()
                 assert task.call_count == 1
                 assert isinstance(task.call_args[0][0], Context)
 
         class hosts_flag_set:
             def parameterization_per_host(self):
-                task = _execute_task(val="host1,host2,host3")
+                task = _execute(val="host1,host2,host3")
                 assert task.call_count == 3
                 assert isinstance(task.call_args[0][0], Connection)
 
             def post_tasks_happen_once_only(self):
                 post = Mock()
-                task = _execute_task(val="host1,host2,host3", post=Task(post))
+                task = _execute(val="host1,host2,host3", post=Task(post))
                 assert task.call_count == 3
                 assert post.call_count == 1
 
         class remainder:
             def raises_NothingToDo_when_no_hosts(self):
-                skip()
+                with raises(NothingToDo):
+                    _execute(remainder="whatever")
 
             def creates_anonymous_call_per_host(self):
                 skip()
