e66a049d307ae3989df8c3280722df3941d9758e
Jeff Forcier <jeff@bitprophet.org>
2022-04-03 21:40:27 -0400

    Excise Fabric3 mentions from metadata, plus related tweaks
    Mostly adding Python 3.6-3.8 to classifiers.
    
    (We don't even test on 3.4/3.5 but whatever, probably still works there...)

--- setup.py
+++ setup.py
@@ -8,16 +8,8 @@ from fabric.version import get_version
 
 
 long_description = """
-Fabric3 is a fork of `Fabric <http://fabfile.org>`_ to provide compatability
-with Python 3.4+. The port still works with Python 2.7.
-
-The goal is to stay 100% compatible with the original Fabric.  Any new releases
-of Fabric will also be released here.  Please file issues for any differences
-you find. Known differences are `documented on github
-<https://github.com/mathiasertl/fabric/>`.
-
 To find out what's new in this version of Fabric, please see `the changelog
-<http://fabfile.org/changelog.html>`_ of the original Fabric.
+<http://fabfile.org/changelog.html>`_.
 
 For more information, please see the Fabric website or execute ``fab --help``.
 """
@@ -26,15 +18,12 @@ install_requires=['paramiko>=2.0,<3.0', 'six>=1.10.0']
 
 
 setup(
-    name='Fabric3',
+    name='Fabric',
     version=get_version('short'),
-    description='Fabric is a simple, Pythonic tool for remote execution and deployment (py2.7/py3.4+ compatible fork).',
+    description='Fabric is a simple, Pythonic tool for remote execution and deployment.',
     long_description=long_description,
     author='Jeff Forcier',
     author_email='jeff@bitprophet.org',
-    maintainer='Mathias Ertl',
-    maintainer_email='mati@er.tl',
-    url='https://github.com/mathiasertl/fabric/',
     packages=find_packages(),
     test_suite='nose.collector',
     tests_require=['nose<2.0', 'fudge<1.0', 'jinja2<3.0'],
@@ -59,6 +48,9 @@ setup(
           'Programming Language :: Python :: 3',
           'Programming Language :: Python :: 3.4',
           'Programming Language :: Python :: 3.5',
+          'Programming Language :: Python :: 3.6',
+          'Programming Language :: Python :: 3.7',
+          'Programming Language :: Python :: 3.8',
           'Topic :: Software Development',
           'Topic :: Software Development :: Build Tools',
           'Topic :: Software Development :: Libraries',
