5fb2775e4b209f07ed1a352cf25ffceb4f1c8991
Jeff Forcier <jeff@bitprophet.org>
2023-02-16 14:28:55 -0500

    Add 3-only classifier

--- setup.py
+++ setup.py
@@ -89,6 +89,7 @@ setuptools.setup(
         "Operating System :: Microsoft :: Windows",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3 :: Only",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
